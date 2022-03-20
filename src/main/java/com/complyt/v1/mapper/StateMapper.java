21311b106065846f8aa22f9ce24c811b6bfa9c03
Alexander Tilkin <alextilk@gmail.com>
2022-03-20 18:17:14 +0200

    I enabled mapstruct with Lombok

--- src/main/java/com/complyt/v1/mapper/StateMapper.java
+++ src/main/java/com/complyt/v1/mapper/StateMapper.java
@@ -1,21 +1,18 @@
 package com.complyt.v1.mapper;
 
 import com.complyt.domain.State;
-
 import com.complyt.v1.model.StateDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface StateMapper {
+
     StateMapper INSTANCE = Mappers.getMapper( StateMapper.class );
 
-    @Mappings ({
-            @Mapping(source = "state.salesTaxRate", target = "salesTaxRate"),
-            @Mapping(source = "state.name", target = "name")
-    })
+    @Mapping(source = "salesTaxRate", target = "salesTaxRate")
+    @Mapping(source = "name", target = "name")
     StateDto stateToStateDto(State state);
 
 //    @Mappings({
