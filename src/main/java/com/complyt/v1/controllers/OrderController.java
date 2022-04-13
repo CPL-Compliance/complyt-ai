91ca902891b5acf5f90a4b0c9e8f005881d29b9b
eyaltamsot <eyaltams@gmail.com>
2022-04-13 16:07:10 +0300

    updaing orderDto and controller

--- src/main/java/com/complyt/v1/controllers/OrderController.java
+++ src/main/java/com/complyt/v1/controllers/OrderController.java
@@ -1,29 +1,48 @@
 package com.complyt.v1.controllers;
 
+import com.complyt.domain.Customer;
 import com.complyt.domain.Order;
 import com.complyt.facades.OrderFacade;
+import com.complyt.repositories.exceptions.OperationFailedException;
+import com.complyt.v1.mappers.CustomerMapper;
 import com.complyt.v1.mappers.OrderMapper;
+import com.complyt.v1.model.CustomerDto;
 import com.complyt.v1.model.OrderDto;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping(OrderController.BASE_URL)
 public class OrderController {
-    public static final String BASE_URL = "/v1/order";
+    public static final String BASE_URL = "/v1/orders";
 
     @NonNull
     private OrderFacade orderFacade;
 
-    @PutMapping("")
-    public Order createOrder(@RequestBody OrderDto orderDto) {
-        Order createdOPrder = orderFacade.create(OrderMapper.INSTANCE.orderDtoToOrder(orderDto));
+//    @PutMapping("")
+//    public Order createOrder(@RequestBody OrderDto orderDto) {
+//        Order createdOPrder = orderFacade.create(OrderMapper.INSTANCE.orderDtoToOrder(orderDto));
+//
+//        return createdOPrder;
+//    }
 
-        return createdOPrder;
+    @PutMapping("")
+    public Mono<OrderDto> upsert(@RequestBody OrderDto orderDto) {
+        try {
+            Order order = OrderMapper.INSTANCE.orderDtoToOrder(orderDto);
+            Mono<Order> orderMono = orderFacade.upsert(order);
+
+            return orderMono.map(orderItem -> OrderMapper.INSTANCE.orderToOrderDto(orderItem));
+        } catch (OperationFailedException operationFailedException) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, orderDto.toString(), operationFailedException);
+        }
     }
 }
--- src/main/java/com/complyt/v1/model/OrderDto.java
+++ src/main/java/com/complyt/v1/model/OrderDto.java
@@ -1,5 +1,6 @@
 package com.complyt.v1.model;
 
+import com.complyt.domain.Address;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.*;
@@ -11,9 +12,9 @@ import org.bson.types.ObjectId;
 @EqualsAndHashCode
 @NoArgsConstructor
 public class OrderDto {
-    private String id;
-    private ObjectId customer_id;
-    private String type;
-    private int units;
-    private int price;
+    private String externalId;
+    private Address billingAddress;
+    private Address shippingAddress;
+    private ObjectId customerId;
+
 }
