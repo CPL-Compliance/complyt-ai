bbfe20ab547c7ce4542a080238f741cac32813e3
eyaltamsot <eyaltams@gmail.com>
2022-04-20 16:12:12 +0300

    PR fixes

--- src/test/java/com/complyt/v1/model/CustomerDtoTest.java
+++ src/test/java/com/complyt/v1/model/CustomerDtoTest.java
@@ -1,2 +1,53 @@
-package com.complyt.v1.model;public class OrderDtoTest {
+package com.complyt.v1.model;
+
+import com.complyt.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+public class CustomerDtoTest {
+
+    private CustomerDto customerDto;
+    private CustomerDto anotherCustomerDto;
+
+
+
+    @BeforeEach
+    void setUp() {
+        String id = UUID.randomUUID().toString();
+        String externalId = UUID.randomUUID().toString();
+        String name = "Existing Customer";
+        AddressDto address = new AddressDto("City", "Country", "County", "State", "Street", "Zip");
+        customerDto = new CustomerDto(id, externalId, name, address);
+        anotherCustomerDto = new CustomerDto(customerDto.getId(),customerDto.getExternalId(),customerDto.getName(),customerDto.getAddress());
+    }
+
+    @Test
+    void equals_IdenticalStates_Equal() {
+        assertEquals(customerDto, anotherCustomerDto);
+    }
+
+    @Test
+    void hashCode_IdenticalStates_Equal() {
+        assertEquals(customerDto.hashCode(), anotherCustomerDto.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        String referenceString = "CustomerDto(id=" + customerDto.getId() + ", externalId=" + customerDto.getExternalId() + ", name=" + customerDto.getName() + ", address=" + customerDto.getAddress() + ")";
+
+        assertEquals(referenceString, customerDto.toString());
+    }
 }
