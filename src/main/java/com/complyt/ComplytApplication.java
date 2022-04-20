1a9d493036c4c7d9f1a71ac4f0e5e5c3c231c904
Alexander Tilkin <alextilk@gmail.com>
2022-04-20 10:29:52 +0300

    Update ComplytApplication.java

--- src/main/java/com/complyt/ComplytApplication.java
+++ src/main/java/com/complyt/ComplytApplication.java
@@ -10,11 +10,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class ComplytApplication implements CommandLineRunner {
 
-//    @Bean
-//    public RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
-
     public static void main(String[] args) {
         SpringApplication.run(ComplytApplication.class, args);
     }
