7262278520e2e230dd0afec157c60a9370dd91ad
eyaltamsot <eyaltams@gmail.com>
2022-05-09 16:38:52 +0300

    configuration files

---
+++ plugins/netsuite/customer_config.js
@@ -0,0 +1,3 @@
+var customerSettings = {
+    BASE_URL : "https://complyt-test.herokuapp.com/v1/customers/",
+}
---
+++ plugins/netsuite/invoice_config.js
@@ -0,0 +1,6 @@
+var invoiceSettings = {
+    BASE_URL : "https://complyt-test.herokuapp.com/v1/orders",
+    CUSTOMER_BASE_URL: "https://complyt-test.herokuapp.com/v1/customers/",
+    ACTIVE_STATUS: 'ACTIVE',
+    DELETED_STATUS: 'CANCELLED'
+}
