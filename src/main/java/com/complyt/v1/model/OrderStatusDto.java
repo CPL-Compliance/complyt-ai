2a120ea938facac6ba49ece246203f8f204c0970
eyaltamsot <eyaltams@gmail.com>
2022-05-09 20:35:52 +0300

    cleaning orderStatusDto

--- src/main/java/com/complyt/v1/model/OrderStatusDto.java
+++ src/main/java/com/complyt/v1/model/OrderStatusDto.java
@@ -1,27 +1,7 @@
 package com.complyt.v1.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
-import java.util.stream.Stream;
-
 public enum OrderStatusDto {
     ACTIVE,
     CANCELLED
 }
-//    private String status;
-//
-//    private OrderStatusDto(String status) {
-//        this.status=status;
-//    }
-//
-//    @JsonCreator
-//    public static OrderStatusDto decode(final String status) {
-//        return Stream.of(OrderStatusDto.values()).filter(targetEnum -> targetEnum.status.equals(status)).findFirst().orElse(null);
-//    }
-//
-//    @JsonValue
-//    public String getStatus() {
-//        return status;
-//    }
-//}
+
