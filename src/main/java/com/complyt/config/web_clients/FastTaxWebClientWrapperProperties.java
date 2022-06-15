641b2a6716a9b07df7cf1e6471fe0ad71dc679f2
eyaltamsot <eyaltams@gmail.com>
2022-06-15 15:44:18 +0300

    changing url of fast tax service

--- src/main/java/com/complyt/config/web_clients/FastTaxWebClientWrapperProperties.java
+++ src/main/java/com/complyt/config/web_clients/FastTaxWebClientWrapperProperties.java
@@ -4,7 +4,7 @@ import org.javatuples.Pair;
 
 public class FastTaxWebClientWrapperProperties {
     public static final String SCHEME = "https";
-    public static final String HOST = "trial.serviceobjects.com";
-    public static final String PATH = "ft/web.svc/JSON/GetBestMatch";
-    public static final Pair<String, String> KEY = new Pair<>("licensekey", "WS19-KRF3-JGD1");
+    public static final String HOST = "ws.serviceobjects.com";
+    public static final String PATH = "FT/web.svc/json/GetBestMatch";
+    public static final Pair<String, String> KEY = new Pair<>("licensekey", "WS19-HSN4-DXJ2");
 }
