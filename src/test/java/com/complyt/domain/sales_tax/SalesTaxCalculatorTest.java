949449596ef5ea2c59da5501ccdb50c7ca24f5f3
eyaltamsot <eyaltams@gmail.com>
2022-06-21 17:37:33 +0300

    more tests to sales tax calculator

--- src/test/java/com/complyt/domain/sales_tax/SalesTaxCalculatorTest.java
+++ src/test/java/com/complyt/domain/sales_tax/SalesTaxCalculatorTest.java
@@ -4,6 +4,7 @@ import com.complyt.business.sales_tax.SalesTaxCalculator;
 import com.complyt.domain.Item;
 import com.complyt.domain.sales_tax.product_classification.CalculationType;
 import com.complyt.domain.sales_tax.product_classification.JurisdictionalSalesTaxRules;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,12 +26,19 @@ public class SalesTaxCalculatorTest {
     @InjectMocks
     SalesTaxCalculator salesTaxCalculator;
 
+    JurisdictionalSalesTaxRules jurisdictionalSalesTaxRules;
+
+    @BeforeEach
+    void setUp() {
+        jurisdictionalSalesTaxRules = new JurisdictionalSalesTaxRules("California","CA",true,true,
+                CalculationType.FIXED,"description",0.5f,null);
+    }
+
     @Test
     void calculate_SalesTaxBeingCalculated_SalesTaxAmountReturned(){
         // Given
         SalesTaxRate salesTaxRate = new SalesTaxRate(0.5f,0.5f,0.5f,0.5f,0.5f,0.5f);
-        JurisdictionalSalesTaxRules jurisdictionalSalesTaxRules = new JurisdictionalSalesTaxRules("California","CA",true,false,
-                CalculationType.FIXED,"description",0f,null);
+
         List<Item> items = new ArrayList<Item>(){{
                 add(new Item(1000,2,2000,"description","name","taxCode",jurisdictionalSalesTaxRules, salesTaxRate,false,0));
                 add(new Item(3000,3,9000,"description","name","taxCode",jurisdictionalSalesTaxRules, salesTaxRate,false,0));
@@ -57,6 +65,26 @@ public class SalesTaxCalculatorTest {
             add(new Item(1000,2,2000,"description","name","taxCode",null, salesTaxRate,true,0.5f));
         }};
         float amount = items.get(0).getTotalPrice() * 0.5f;
+
+        // When
+        float salesTaxAmountReturnedFromCalculation = salesTaxCalculator.calculate(items);
+
+        // Then
+        assertEquals(amount,salesTaxAmountReturnedFromCalculation);
+
+    }
+
+    @Test
+    void calculate_JurisdictionalSalesTaxRulesIsCalculatedByPercentage_SalesTaxAmountReturned(){
+        // Given
+        SalesTaxRate salesTaxRate = new SalesTaxRate(0.5f,0.5f,0.5f,0.5f,0.5f,0.5f);
+        JurisdictionalSalesTaxRules jurisdictionalSalesTaxRulesByPercentage = jurisdictionalSalesTaxRules.withCalculationType(CalculationType.PERCENTAGE);
+        List<Item> items = new ArrayList<Item>(){{
+            add(new Item(1000,2,2000,"description","name","taxCode",
+                    jurisdictionalSalesTaxRulesByPercentage, salesTaxRate,false,0.5f));
+        }};
+        float totalPrice = items.get(0).getTotalPrice() * jurisdictionalSalesTaxRulesByPercentage.getCalculationValue();
+        float amount = totalPrice * items.get(0).getSalesTaxRate().getTaxRate();
 
         // When
         float salesTaxAmountReturnedFromCalculation = salesTaxCalculator.calculate(items);
