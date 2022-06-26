d7b7e142ab451ceb68fe0553732e757908a3c997
eyaltamsot <eyaltams@gmail.com>
2022-06-26 12:28:23 +0300

    adding-id-to-orderDto

--- src/main/java/com/complyt/v1/model/OrderDto.java
+++ src/main/java/com/complyt/v1/model/OrderDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @With
 @Schema(name = "Order")
 public class OrderDto {
+    private String id;
     private String externalId;
     private List<ItemDto> items;
     private AddressDto billingAddress;
