ee51727832053a5c5424bc86ea1cc9109f8f96df
eyaltamsot <eyaltams@gmail.com>
2022-07-24 20:34:53 +0300

    optimizing imports

--- src/main/java/com/complyt/ComplytApplication.java
+++ src/main/java/com/complyt/ComplytApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @SpringBootApplication
 public class ComplytApplication implements CommandLineRunner {
 
