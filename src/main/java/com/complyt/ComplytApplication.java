f4dbf0f514743441f0d51f9aec6e747a6ee9fbda
eyaltamsot <eyaltams@gmail.com>
2022-08-02 09:23:00 +0300

    cleaning main

--- src/main/java/com/complyt/ComplytApplication.java
+++ src/main/java/com/complyt/ComplytApplication.java
@@ -3,15 +3,10 @@ package com.complyt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
-
-import static java.time.temporal.TemporalAdjusters.firstDayOfYear;
 @SpringBootApplication
 public class ComplytApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        System.out.println(LocalDateTime.now().minusYears(2).with(firstDayOfYear()).withHour(0).withMinute(0).withSecond(0).withNano(0));
         SpringApplication.run(ComplytApplication.class, args);
     }
 
