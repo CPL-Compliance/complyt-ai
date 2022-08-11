6386404730b9a34c9f208797be0bc86390d829af
eyaltamsot <eyaltams@gmail.com>
2022-08-11 17:53:16 +0300

    deleting unneccessary imports

--- src/main/java/com/complyt/business/utils/data_fetcher/TransactionZipTaxCountyFetcher.java
+++ src/main/java/com/complyt/business/utils/data_fetcher/TransactionZipTaxCountyFetcher.java
@@ -2,12 +2,10 @@ package com.complyt.business.utils.data_fetcher;
 
 import com.complyt.business.sales_tax.sales_tax_web_clients.SalesTaxWebClientWrapper;
 import com.complyt.domain.Address;
-import com.complyt.domain.Transaction;
 import com.complyt.domain.sales_tax.zip_tax.ZipTaxData;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
--- src/main/java/com/complyt/facades/TransactionFacade.java
+++ src/main/java/com/complyt/facades/TransactionFacade.java
@@ -16,6 +16,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 public class TransactionFacade {
+
     @NonNull
     @Qualifier("transactionServiceImpl")
     private TransactionService transactionService;
