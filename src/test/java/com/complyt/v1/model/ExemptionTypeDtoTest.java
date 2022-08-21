87527e52c6bbcd940103f3ed0152bdcf2868e951
eyaltamsot <eyaltams@gmail.com>
2022-08-21 12:34:15 +0300

    tests to exemptionTypeDto so codecov will allow merging

---
+++ src/test/java/com/complyt/v1/model/ExemptionTypeDtoTest.java
@@ -0,0 +1,19 @@
+package com.complyt.v1.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExemptionTypeDtoTest {
+
+    @Test
+    void fullyTypeInit() {
+        ExemptionTypeDto exemptionTypeDto = ExemptionTypeDto.FULLY;
+        assertEquals(exemptionTypeDto,ExemptionTypeDto.FULLY);
+    }
+
+    @Test
+    void partiallyTypeInit() {
+        ExemptionTypeDto exemptionTypeDto = ExemptionTypeDto.PARTIALLY;
+        assertEquals(exemptionTypeDto,ExemptionTypeDto.PARTIALLY);
+    }
+}
