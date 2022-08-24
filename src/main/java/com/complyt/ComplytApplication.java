eb1e34a5c76e97f3284a674a5ec0bf006b72f61b
Alexander Tilkin <alextilk@gmail.com>
2022-08-24 21:55:52 +0300

    Test

--- src/main/java/com/complyt/ComplytApplication.java
+++ src/main/java/com/complyt/ComplytApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComplytApplication implements CommandLineRunner {
     // REMOVE
+    // Another change
     public static void main(String[] args) {
         SpringApplication.run(ComplytApplication.class, args);
     }
