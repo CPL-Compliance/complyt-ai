d1947f0459581c9181b19182aba54ec3ca58f641
Alexander Tilkin <alextilk@gmail.com>
2022-08-25 17:16:50 +0300

    Removing comments

--- src/main/java/com/complyt/ComplytApplication.java
+++ src/main/java/com/complyt/ComplytApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComplytApplication implements CommandLineRunner {
-    // REMOVE
-    // Another change
     public static void main(String[] args) {
         SpringApplication.run(ComplytApplication.class, args);
     }
