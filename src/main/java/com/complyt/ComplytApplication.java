32703ff0228e8f1dfb2b2be7d1b9ab44eda245fd
Alexander Tilkin <alextilk@gmail.com>
2022-09-01 16:56:50 +0300

    Fixing project

--- src/main/java/com/complyt/ComplytApplication.java
+++ src/main/java/com/complyt/ComplytApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class TransactionsService implements CommandLineRunner {
+public class ComplytApplication implements CommandLineRunner {
     // This is a test
     public static void main(String[] args) {
-        SpringApplication.run(TransactionsService.class, args);
+        SpringApplication.run(ComplytApplication.class, args);
     }
 
     @Override
