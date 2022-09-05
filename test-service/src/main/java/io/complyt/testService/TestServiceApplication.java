41e6ba839e28d7d1a8c92d9b1869d5407c4ed30f
Alexander Tilkin <alextilk@gmail.com>
2022-09-05 14:13:28 +0300

    Test coverage

--- test-service/src/main/java/io/complyt/testService/TestServiceApplication.java
+++ test-service/src/main/java/io/complyt/testService/TestServiceApplication.java
@@ -3,10 +3,12 @@ package io.complyt.testService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class TestServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(ArrayList<String> args) {
 		SpringApplication.run(TestServiceApplication.class, args);
 	}
 
--- test-service/src/test/java/io/complyt/testService/TestServiceApplicationTests.java
+++ test-service/src/test/java/io/complyt/testService/TestServiceApplicationTests.java
@@ -1,13 +1,21 @@
 package io.complyt.testService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 class TestServiceApplicationTests {
 
+	@Autowired
+
+	TestServiceApplication testServiceApplication;
+
 	@Test
 	void contextLoads() {
+		testServiceApplication.main(new ArrayList<String>());
 	}
 
 }
