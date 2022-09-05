df7897cecfbe1e1e1d6c55295777d6fb5267c406
Alexander Tilkin <alextilk@gmail.com>
2022-09-05 22:00:41 +0300

    Working on the config file

--- test-service/src/main/java/io/complyt/testService/Controller.java
+++ test-service/src/main/java/io/complyt/testService/Controller.java
@@ -1,8 +1,10 @@
 package io.complyt.testService;
 
+import lombok.Generated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Generated
 @RestController
 public class Controller {
 
