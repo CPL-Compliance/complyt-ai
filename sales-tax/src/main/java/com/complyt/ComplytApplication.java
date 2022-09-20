3f6ad5de17b9ecf81ff10c3d362c43563b7898a8
Alexander Tilkin <alextilk@gmail.com>
2022-09-20 16:36:40 +0300

    Trying to solve the denial of the token renewal

--- sales-tax/src/main/java/com/complyt/ComplytApplication.java
+++ sales-tax/src/main/java/com/complyt/ComplytApplication.java
@@ -1,9 +1,12 @@
 package com.complyt;
 
 import com.complyt.annotations.Generated;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @Generated
 @SpringBootApplication
@@ -13,7 +16,21 @@ public class ComplytApplication implements CommandLineRunner {
         SpringApplication.run(ComplytApplication.class, args);
     }
 
+    @Value("${fast-tax-api-key}")
+    String fastTaxKey;
+
+    @Value("${zip-tax-api-key}")
+    String zipTaxKey;
+
     @Override
     public void run(String... args) {
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("##########################");
+        System.out.println(fastTaxKey);
+        System.out.println(zipTaxKey);
+        System.out.println("##########################");
     }
 }
