8330e506e2ce0c0bdb1e4434d917ee89b6e04e76
eyaltamsot <eyaltams@gmail.com>
2022-09-21 10:26:31 +0300

    removing unnecessary functions from mappers

--- sales-tax/src/main/java/com/complyt/v1/mappers/ExemptionMapper.java
+++ sales-tax/src/main/java/com/complyt/v1/mappers/ExemptionMapper.java
@@ -1,10 +1,7 @@
 package com.complyt.v1.mappers;
 
-import com.complyt.domain.State;
-import com.complyt.domain.TimeStamps;
-import com.complyt.domain.customer.exemption.*;
-import com.complyt.v1.model.TimeStampsDto;
-import com.complyt.v1.model.customer.exemption.*;
+import com.complyt.domain.customer.exemption.Exemption;
+import com.complyt.v1.model.customer.exemption.ExemptionDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueMappingStrategy;
 import org.mapstruct.factory.Mappers;
@@ -14,34 +11,5 @@ public interface ExemptionMapper {
     ExemptionMapper INSTANCE = Mappers.getMapper(ExemptionMapper.class);
 
     Exemption exemptionDtoToExemption(ExemptionDto exemptionDto);
-
     ExemptionDto exemptionToExemptionDto(Exemption exemption);
-
-    State stateDtoToState(StateDto stateDto);
-
-    StateDto stateToStateDto(State state);
-
-    Classification classificationDtoToClassification(ClassificationDto classificationDto);
-
-    ClassificationDto classificationToClassificationDto(Classification classification);
-
-    ValidationDates validationDatesDtoToValidationDates(ValidationDatesDto validationDatesDto);
-
-    ValidationDatesDto validationDatesToValidationDatesDto(ValidationDates validationDates);
-
-    TimeStamps timeStampsDtoToTimeStamps(TimeStampsDto timeStampsDto);
-
-    TimeStampsDto timeStampsToTimeStampsDto(TimeStamps timeStamps);
-
-    Status statusDtoToStatus(StatusDto statusDto);
-
-    StatusDto statusToStatusDto(Status status);
-
-    Certificate certificateDtoToCertificate(CertificateDto certificateDto);
-
-    CertificateDto certificateToCertificateDto(Certificate certificate);
-
-    ExemptionType exemptionTypeDtoToExemptionType(ExemptionTypeDto exemptionTypeDto);
-
-    ExemptionTypeDto exemptionTypeToExemptionTypeDto(ExemptionType exemptionType);
 }
--- sales-tax/src/main/java/com/complyt/v1/mappers/TransactionMapper.java
+++ sales-tax/src/main/java/com/complyt/v1/mappers/TransactionMapper.java
@@ -1,53 +1,15 @@
 package com.complyt.v1.mappers;
 
-import com.complyt.domain.Item;
 import com.complyt.domain.Transaction;
-import com.complyt.domain.TransactionStatus;
-import com.complyt.domain.customer.CustomerType;
-import com.complyt.domain.sales_tax.SalesTaxRate;
-import com.complyt.v1.model.ItemDto;
 import com.complyt.v1.model.TransactionDto;
-import com.complyt.v1.model.TransactionStatusDto;
-import com.complyt.v1.model.SalesTaxRateDto;
-import com.complyt.domain.*;
-import com.complyt.v1.model.*;
-import com.complyt.v1.model.customer.CustomerTypeDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueMappingStrategy;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 @Mapper(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_NULL)
 public interface TransactionMapper {
     TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
 
     Transaction transactionDtoToTransaction(TransactionDto transactionDto);
-
     TransactionDto transactionToTransactionDto(Transaction transaction);
-
-    SalesTaxRateDto salesTaxRateToSalesTaxRateDto(SalesTaxRate salesTaxRate);
-
-    SalesTaxRate salesTaxRateDtoToSalesTaxRate(SalesTaxRateDto salesTaxRateDto);
-
-    TransactionStatusDto transactionStatusToTransactionStatusDto(TransactionStatus transactionStatus);
-
-    TransactionStatus transactionStatusDtoToTransactionStatus(TransactionStatusDto transactionStatusDto);
-
-    List<Item> itemDtosToItems(List<ItemDto> itemDtos);
-
-    List<ItemDto> itemsToItemDtos(List<Item> items);
-
-    Item itemDtoToItem(ItemDto itemDto);
-
-    ItemDto itemToItemDto(Item item);
-
-    TimeStamps timeStampsDtoToTimeStamps(TimeStampsDto timeStampsDto);
-
-    TimeStampsDto timeStampsToTimeStampsDto(TimeStamps timeStamps);
-
-    CustomerType customerTypeDtoToCustomerType(CustomerTypeDto customerTypeDto);
-
-    CustomerTypeDto customerTypeToCustomerTypeDto(CustomerType customerType);
-
 }
