6e4822fe3c47124533b85077b2dde678616a64b0
Alexander Tilkin <alextilk@gmail.com>
2022-09-25 10:55:24 +0300

    Fixing code

--- sales-tax/src/main/java/com/complyt/ComplytApplication.java
+++ sales-tax/src/main/java/com/complyt/ComplytApplication.java
@@ -1,12 +1,9 @@
 package com.complyt;
 
 import com.complyt.annotations.Generated;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
 
 @Generated
 @SpringBootApplication
