bd9f1136dd51263d9ff009a4b12ae5237cf85ef3
eyaltamsot <eyaltams@gmail.com>
2022-10-18 15:04:26 +0300

    TransactionJurisdictionalRulesInjectorTest

---
+++ sales-tax/src/test/java/com/complyt/business/utils/data_injector/TransactionJurisdictionalRulesInjectorTest.java
@@ -0,0 +1,106 @@
+package com.complyt.business.utils.data_injector;
+
+import com.complyt.business.data_injector.TransactionJurisdictionalRulesInjector;
+import com.complyt.domain.*;
+import com.complyt.domain.nexus.enums.TangibleCategory;
+import com.complyt.domain.nexus.enums.TaxableCategory;
+import com.complyt.domain.sales_tax.SalesTaxRate;
+import com.complyt.domain.sales_tax.product_classification.CalculationType;
+import com.complyt.domain.sales_tax.product_classification.JurisdictionalSalesTaxRules;
+import com.complyt.domain.sales_tax.product_classification.ProductClassification;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TransactionJurisdictionalRulesInjectorTest {
+
+    TransactionJurisdictionalRulesInjector transactionJurisdictionalRulesInjector;
+    Transaction transaction;
+
+    @BeforeEach
+    void setUp() {
+        transaction = createTransaction();
+        transactionJurisdictionalRulesInjector = new TransactionJurisdictionalRulesInjector(transaction);
+    }
+
+    private Transaction createTransaction() {
+        String id = null;
+        String externalId = UUID.randomUUID().toString();
+        ObjectId customerId = new ObjectId();
+        Address billingAddress = new Address("City", "Country", "County", "State", "Street", "Zip");
+        Address shippingAddress = new Address("City", "Country", "County", "CA", "Street", "Zip");
+        ObjectId clientId = new ObjectId();
+        List<Item> items = new ArrayList<>() {
+            {
+                add(new Item(2000, 4, 8000, "description", "name", "C1S1",
+                        null, new SalesTaxRate(0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f), false, 0, TangibleCategory.INTANGIBLE, TaxableCategory.TAXABLE
+                ));
+            }
+        };
+        TimeStamps timeStamps = new TimeStamps(LocalDateTime.now(), LocalDateTime.now());
+        ShippingFee shippingFee = createShippingFee();
+        return new Transaction(id, externalId, items, billingAddress, shippingAddress, customerId, null, null, TransactionStatus.ACTIVE, clientId, timeStamps, timeStamps, TransactionType.INVOICE, shippingFee);
+    }
+
+    private ShippingFee createShippingFee() {
+        return new ShippingFee(false, 0, 1000, null,
+                new SalesTaxRate(0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.5f), "C6S1");
+    }
+
+    private JurisdictionalSalesTaxRules createJurisdictionalSalesTaxRules() {
+        return new JurisdictionalSalesTaxRules("California", "CA", true, false,
+                CalculationType.FIXED, "description", 0, null);
+    }
+
+    private Map<String, ProductClassification> createMapTaxCodesToClassifications() {
+        JurisdictionalSalesTaxRules jurisdictionalSalesTaxRules = createJurisdictionalSalesTaxRules();
+        Map<String, JurisdictionalSalesTaxRules> itemJurisdictionalSalesTaxRulesMap = new HashMap<>() {{
+            put("CA", jurisdictionalSalesTaxRules);
+        }};
+        ProductClassification itemProductClassification = new ProductClassification(UUID.randomUUID().toString()
+                , "C1S1", "item", "title", itemJurisdictionalSalesTaxRulesMap, TangibleCategory.TANGIBLE);
+        Map<String, JurisdictionalSalesTaxRules> shippingJurisdictionalSalesTaxRulesMap = new HashMap<>() {{
+            put("CA", jurisdictionalSalesTaxRules);
+        }};
+        ProductClassification shippingProductClassification = new ProductClassification(UUID.randomUUID().toString()
+                , "C6S1", "item", "title", shippingJurisdictionalSalesTaxRulesMap, TangibleCategory.INTANGIBLE);
+
+        return new HashMap<>() {{
+            put("C1S1", itemProductClassification);
+            put("C6S1", shippingProductClassification);
+        }};
+    }
+
+    @Test
+    void inject_InjectsDataToTransactionWithShippingFee_ReturnsModifiedTransaction() {
+        // Given
+
+        Map<String, ProductClassification> mapTaxCodesToClassifications = createMapTaxCodesToClassifications();
+        ShippingFee shippingFeeWithRules = transaction.getShippingFee().withJurisdictionalSalesTaxRules(createJurisdictionalSalesTaxRules());
+        Item itemWithRules = transaction.getItems().get(0).withJurisdictionalSalesTaxRules(createJurisdictionalSalesTaxRules());
+        List<Item> itemsWithRules = new ArrayList<>() {{
+            add(itemWithRules);
+        }};
+        Transaction transactionWithRules = transaction
+                .withItems(itemsWithRules)
+                .withShippingFee(shippingFeeWithRules);
+
+        // When + Then
+        Mono<Transaction> actualTransactionMono = transactionJurisdictionalRulesInjector.inject(mapTaxCodesToClassifications);
+
+        StepVerifier.create(actualTransactionMono).expectNext(transactionWithRules).verifyComplete();
+    }
+
+}
