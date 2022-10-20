b7f89aa933450640632dca9ef4fea0b7ea0c13ec
eyaltamsot <eyaltams@gmail.com>
2022-10-20 14:38:13 +0300

    from stream to for loop in items ST calculation

--- sales-tax/src/main/java/com/complyt/business/sales_tax/sales_tax_amount/ItemsSalesTaxCalculator.java
+++ sales-tax/src/main/java/com/complyt/business/sales_tax/sales_tax_amount/ItemsSalesTaxCalculator.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @AllArgsConstructor
@@ -17,11 +16,13 @@ public class ItemsSalesTaxCalculator {
 
     public float calculate(@NonNull List<Item> items) {
         log.info("Calculating total sales tax amount for items");
-
-        Optional<Float> amount = items.stream().map(this::calculateSalesTaxAmount).reduce(Float::sum);
+        float amount = 0;
+        for (Item item : items) {
+            amount += calculateSalesTaxAmount(item);
+        }
         log.debug("Items Sales tax amount calculated : " + amount);
 
-        return amount.get();
+        return amount;
     }
 
     private float calculateSalesTaxAmount(Item item) {
