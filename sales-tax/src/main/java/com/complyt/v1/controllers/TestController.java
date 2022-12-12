79c4e4feb2aa0f5def21d0f733cd30c71fe1baf5
Alexander Tilkin <alextilk@gmail.com>
2022-12-12 16:24:21 +0200

    I commented the code that relates to Feign. We will remove this code after we will actually use it in production

--- sales-tax/src/main/java/com/complyt/v1/controllers/TestController.java
+++ sales-tax/src/main/java/com/complyt/v1/controllers/TestController.java
@@ -1,30 +1,30 @@
-package com.complyt.v1.controllers;
-
-import com.complyt.proxies.TestServiceProxy;
-import com.complyt.security.permissions.transaction.TransactionReadPermission;
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-
-@RestController
-public class TestController {
-
-    @Autowired
-    private TestServiceProxy testServiceProxy;
-
-    @Operation(summary = "Testing feign and eureka")
-    @TransactionReadPermission
-    @GetMapping("/test/hi")
-    public Mono<String> test() {
-        return testServiceProxy.hi().map(s -> s);
-    }
-
-    @Operation(summary = "Testing feign and eureka")
-    @TransactionReadPermission
-    @GetMapping("/test/bye")
-    public Mono<String> bue() {
-        return testServiceProxy.bye();
-    }
-}
+//package com.complyt.v1.controllers;
+//
+//import com.complyt.proxies.TestServiceProxy;
+//import com.complyt.security.permissions.transaction.TransactionReadPermission;
+//import io.swagger.v3.oas.annotations.Operation;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//public class TestController {
+//
+//    @Autowired
+//    private TestServiceProxy testServiceProxy;
+//
+//    @Operation(summary = "Testing feign and eureka")
+//    @TransactionReadPermission
+//    @GetMapping("/test/hi")
+//    public Mono<String> test() {
+//        return testServiceProxy.hi().map(s -> s);
+//    }
+//
+//    @Operation(summary = "Testing feign and eureka")
+//    @TransactionReadPermission
+//    @GetMapping("/test/bye")
+//    public Mono<String> bue() {
+//        return testServiceProxy.bye();
+//    }
+//}
