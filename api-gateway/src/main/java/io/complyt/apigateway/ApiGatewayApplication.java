88b7b31375b402237b5c1382781f08936ecbfead
Alexander Tilkin <alextilk@gmail.com>
2022-12-14 18:52:34 +0200

    Moved the annotation

--- api-gateway/src/main/java/io/complyt/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/io/complyt/apigateway/ApiGatewayApplication.java
@@ -7,9 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@Generated
 public class ApiGatewayApplication {
-
-    @Generated
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
