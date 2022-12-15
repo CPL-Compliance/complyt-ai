6c5bb062a938e8007b36adb78bced2ec758bf0f3
Alexander Tilkin <alextilk@gmail.com>
2022-12-15 07:20:32 +0200

    Small changes
    Signed-off-by: Alexander Tilkin <alextilk@gmail.com>

--- api-gateway/src/test/java/io/complyt/apigateway/ApiGatewayApplicationTest.java
+++ api-gateway/src/test/java/io/complyt/apigateway/ApiGatewayApplicationTest.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @Configuration
 class SpringSecurityTestConfiguration {
 
 }
 
-// I use SpringSecurityTestConfiguration to prevent SpringBootTest from load the production configuration files.
+// I use SpringSecurityTestConfiguration to prevent SpringBootTest from loading the production configuration files.
 @SpringBootTest(classes = SpringSecurityTestConfiguration.class)
 class ApiGatewayApplicationTest {
 
