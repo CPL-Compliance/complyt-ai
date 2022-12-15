f063db914ce12f8994c4dc3616cd31622261ff77
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2022-12-15 09:57:44 +0200

    changing to ignoring the exceptions

--- sales-tax/src/main/java/com/complyt/v1/model/TimeStampsDto.java
+++ sales-tax/src/main/java/com/complyt/v1/model/TimeStampsDto.java
@@ -1,7 +1,10 @@
 package com.complyt.v1.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.With;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
@@ -29,26 +32,23 @@ public class TimeStampsDto {
         String localDateTime = null;
         try {
             LocalDateTime localDate = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE).atTime(0, 0, 0);
-            localDateTime = localDate.toString();
             log.debug("Input is a LocalDate: " + localDateTime);
-        } catch (Exception e) {
-            log.debug("Input is not a LocalDate ");
-        }
+
+            return localDate.toString();
+        } catch (Exception ignored) {}
         try {
             LocalDateTime localDate = LocalDateTime.parse(date, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-            localDateTime = localDate.toString();
             log.debug("Input is a LocalDateTime: " + localDateTime);
-        } catch (Exception e) {
-            log.debug("Input is not a LocalDateTime ");
-        }
+
+            return localDate.toString();
+        } catch (Exception ignored) {}
         try {
             LocalDateTime localDate = ZonedDateTime.parse(date, DateTimeFormatter.ISO_ZONED_DATE_TIME).toLocalDateTime();
-            localDateTime = localDate.toString();
             log.debug("Input is a ZonedDateTime: " + localDateTime);
 
-        } catch (Exception e) {
-            log.debug("Input is not a zonedDateTime ");
-        }
+            return localDate.toString();
+        } catch (Exception ignored) {}
+
         return localDateTime;
     }
 
