3d5e09fd6513e07f97938999c69df2b57bce9c32
Alexander Tilkin <alextilk@gmail.com>
2022-12-27 18:45:19 +0200

    UT WIP

--- filing/src/main/java/io/complyt/filing/v1/handler/LinkHandler.java
+++ filing/src/main/java/io/complyt/filing/v1/handler/LinkHandler.java
@@ -7,21 +7,19 @@ import io.complyt.filing.v1.model.LinkDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-@RestController
+@Service
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @SecurityRequirement(name = "bearerAuth")
 @Tag(name = "Link", description = "This is the Links controller")
 public class LinkHandler {
