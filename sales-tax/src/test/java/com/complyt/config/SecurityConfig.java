ed096a2fd0a2b61147a17896f496269e5b0beae6
Alexander Tilkin <alextilk@gmail.com>
2023-01-24 15:46:19 +0200

    I removed comments from the SecurityConfig class in the Test section

--- sales-tax/src/test/java/com/complyt/config/SecurityConfig.java
+++ sales-tax/src/test/java/com/complyt/config/SecurityConfig.java
@@ -8,29 +8,4 @@ import org.springframework.security.web.server.util.matcher.ServerWebExchangeMat
 
 @Configuration
 public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-//        // CORS
-//
-//        // CSRF
-//        http.csrf().requireCsrfProtectionMatcher(serverWebExchange -> ServerWebExchangeMatchers
-//                .pathMatchers("/token/**")
-//                .matches(serverWebExchange));
-//
-//        // Authentication and Authorization
-//        http.authorizeExchange()
-//                .pathMatchers("/actuator/health",
-//                        "/v3/api-docs/**",
-//                        "/webjars/**",
-//                        "/swagger-ui*/**"
-//                ).permitAll()
-//                .pathMatchers("/actuator/**").hasAuthority("SCOPE_read:actuator")
-//                .anyExchange().authenticated();
-//
-//        // OAuth2
-////        http.oauth2ResourceServer().jwt();
-//
-//        return http.build();
-//    }
 }
