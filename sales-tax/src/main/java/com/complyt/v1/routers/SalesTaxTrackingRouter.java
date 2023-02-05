a4af1a1804ce46e3b743650f87b514eeaae4bc0a
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-02-05 09:58:52 +0200

    tests to sales tax tracking

--- sales-tax/src/main/java/com/complyt/v1/routers/SalesTaxTrackingRouter.java
+++ sales-tax/src/main/java/com/complyt/v1/routers/SalesTaxTrackingRouter.java
@@ -4,9 +4,8 @@ import com.complyt.v1.handlers.SalesTaxTrackingHandler;
 import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
 
 @Configuration
 public class SalesTaxTrackingRouter {
@@ -14,13 +13,39 @@ public class SalesTaxTrackingRouter {
     public static final String BASE_URL = "/v1/nexus";
 
     @Bean
-    public RouterFunction<ServerResponse> salesTaxTrackingRoute(@NonNull final SalesTaxTrackingHandler salesTaxTrackingHandler) {
-        return RouterFunctions.route()
-                .path(BASE_URL, builder -> builder
-                        .GET("/state/{state}", salesTaxTrackingHandler::getOne)
-                        .GET("/complytId/{complytId}", salesTaxTrackingHandler::getByComplytId)
-                        .GET("", salesTaxTrackingHandler::getAll)
-                        .PUT("/state/{state}", salesTaxTrackingHandler::upsert))
-                .build();
+    public RouterFunction<ServerResponse> getSalesTaxTrackingByStateRouterFunction(@NonNull final SalesTaxTrackingHandler salesTaxTrackingHandler) {
+        RequestPredicate getSalesTaxTrackingRoute = RequestPredicates
+                .GET(BASE_URL + "/state/{state}")
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON));
+
+        return RouterFunctions.route(getSalesTaxTrackingRoute, salesTaxTrackingHandler::getOne);
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> getSalesTaxTrackingByComplytIdRouterFunction(@NonNull final SalesTaxTrackingHandler salesTaxTrackingHandler) {
+        RequestPredicate getSalesTaxTrackingRoute = RequestPredicates
+                .GET(BASE_URL + "/complytId/{complytId}")
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON));
+
+        return RouterFunctions.route(getSalesTaxTrackingRoute, salesTaxTrackingHandler::getByComplytId);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getAllSalesTaxTrackingRouterFunction(@NonNull final SalesTaxTrackingHandler salesTaxTrackingHandler) {
+        RequestPredicate getSalesTaxTrackingRoute = RequestPredicates
+                .GET(BASE_URL)
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON));
+
+        return RouterFunctions.route(getSalesTaxTrackingRoute, salesTaxTrackingHandler::getAll);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> upsertSalesTaxTrackingRouterFunction(@NonNull final SalesTaxTrackingHandler salesTaxTrackingHandler) {
+        RequestPredicate getSalesTaxTrackingRoute = RequestPredicates
+                .PUT(BASE_URL + "/state/{state}")
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON));
+
+        return RouterFunctions.route(getSalesTaxTrackingRoute, salesTaxTrackingHandler::upsert);
+    }
+
 }
