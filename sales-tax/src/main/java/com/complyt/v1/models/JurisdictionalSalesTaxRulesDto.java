f76a6168abe340eb48217a0f5672e638abc4317f
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-02-09 14:33:04 +0200

    added @with to JurisdictionalSalesTaxRulesDto to check codecov report of the file

--- sales-tax/src/main/java/com/complyt/v1/models/JurisdictionalSalesTaxRulesDto.java
+++ sales-tax/src/main/java/com/complyt/v1/models/JurisdictionalSalesTaxRulesDto.java
@@ -3,13 +3,11 @@ package com.complyt.v1.models;
 import com.complyt.domain.sales_tax.product_classification.CalculationType;
 import com.complyt.domain.sales_tax.product_classification.CitySalesTaxRules;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.Map;
 
+@With
 @Schema(name = "JurisdictionalSalesTaxRules")
 public record JurisdictionalSalesTaxRulesDto(String name, String abbreviation, boolean taxable,
                                              boolean specialTreatment, CalculationType calculationType,
