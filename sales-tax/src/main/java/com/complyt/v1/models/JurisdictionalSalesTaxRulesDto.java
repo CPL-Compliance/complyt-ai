8d748f847fbbd7ec109ddfad9029ee0ac2776185
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-02-12 11:38:24 +0200

    added generated

--- sales-tax/src/main/java/com/complyt/v1/models/JurisdictionalSalesTaxRulesDto.java
+++ sales-tax/src/main/java/com/complyt/v1/models/JurisdictionalSalesTaxRulesDto.java
@@ -3,9 +3,11 @@ package com.complyt.v1.models;
 import com.complyt.domain.sales_tax.product_classification.CalculationType;
 import com.complyt.domain.sales_tax.product_classification.CitySalesTaxRules;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Generated;
 
 import java.util.Map;
 
+@Generated
 @Schema(name = "JurisdictionalSalesTaxRules")
 public record JurisdictionalSalesTaxRulesDto(String name, String abbreviation, boolean taxable,
                                              boolean specialTreatment, CalculationType calculationType,
