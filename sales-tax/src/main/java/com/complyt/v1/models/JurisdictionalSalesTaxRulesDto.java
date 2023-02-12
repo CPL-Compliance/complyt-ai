e2ff08bd60affe2094881651a5b1f4e136b89242
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-02-12 11:43:26 +0200

    from generated to with just for testing the behavior

--- sales-tax/src/main/java/com/complyt/v1/models/JurisdictionalSalesTaxRulesDto.java
+++ sales-tax/src/main/java/com/complyt/v1/models/JurisdictionalSalesTaxRulesDto.java
@@ -1,13 +1,15 @@
 package com.complyt.v1.models;
 
+
 import com.complyt.domain.sales_tax.product_classification.CalculationType;
 import com.complyt.domain.sales_tax.product_classification.CitySalesTaxRules;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Generated;
+import lombok.With;
 
 import java.util.Map;
 
-@Generated
+@With
 @Schema(name = "JurisdictionalSalesTaxRules")
 public record JurisdictionalSalesTaxRulesDto(String name, String abbreviation, boolean taxable,
                                              boolean specialTreatment, CalculationType calculationType,
