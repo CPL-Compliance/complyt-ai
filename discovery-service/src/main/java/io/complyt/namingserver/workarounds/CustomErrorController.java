a78e5c3a75c1258178d2924a668ab77a017eaf03
Alexander Tilkin <alextilk@gmail.com>
2023-02-15 15:22:41 +0200

    I added @Generated to avoid test-coverage degradation.

--- discovery-service/src/main/java/io/complyt/namingserver/workarounds/CustomErrorController.java
+++ discovery-service/src/main/java/io/complyt/namingserver/workarounds/CustomErrorController.java
@@ -1,10 +1,16 @@
 package io.complyt.namingserver.workarounds;
 
+import io.complyt.namingserver.annotations.Generated;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+* This is a workaround to overcome Spring Boot 3 bug that prevents from the client to reregister once the
+* discovery service restarts.
+* */
+@Generated
 @RestController
 class CustomErrorController implements ErrorController {
     private static final String ERROR_MAPPING = "/error";
