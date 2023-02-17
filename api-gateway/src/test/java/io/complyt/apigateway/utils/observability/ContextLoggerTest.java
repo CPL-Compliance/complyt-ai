dbfdfbbaa433dbd6523978f4fbc012fc85ce72b8
Alexander Tilkin <alextilk@gmail.com>
2023-02-17 07:54:10 +0200

    Added UTs for ContextLogger and organized the obsevability dependencies

---
+++ api-gateway/src/test/java/io/complyt/apigateway/utils/observability/ContextLoggerTest.java
@@ -0,0 +1,20 @@
+package io.complyt.apigateway.utils.observability;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContextLoggerTest {
+
+    @Test
+    void observeCtx_PrintsLogInContext_ReturnsMonoEmpty() {
+        Logger logger = Logger.getLogger("Test");
+        Mono<Object> actualMono = ContextLogger.observeCtx("Test String", logger::info);
+
+        StepVerifier.create(actualMono).verifyComplete();
+    }
+}
