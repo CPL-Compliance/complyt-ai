f4f638124b643b965746ae940c22d2e21beb0041
Yoav Zinger <yoav@complyt.io>
2023-03-01 11:24:53 +0200

    extended the explanation iso8601regex

--- sales-tax/src/main/java/com/complyt/utils/regex/ISO8601Regex.java
+++ sales-tax/src/main/java/com/complyt/utils/regex/ISO8601Regex.java
@@ -6,6 +6,32 @@ import lombok.experimental.FieldDefaults;
 /*
 Holds a const regular expression to check if datetime is in correct ISO8601 format
 
+Short explanation:
+=================
+the FIRST part checks for the format yyyy-mm-dd:
+# the year must be 4 digits, and month and days must be 2 digits (with 0 in the begging if one digit)
+
+(((20([0-9]{2})|2100)-((0[1-9])|(1[0-2]))-(0[1-9]|1\d|2[0-8])
+# checks up to the 28th of each month (every month have at least 28 days)
+
+(20([0-9]{2})|2100)-(((0[13578]|1[02])-31)|((0[13-9]|1[0-2])-(29|30)))
+# checks for months with 29,30 and 31 days
+
+(20(00|04|[2468][048]|[13579][26])|2100)-02-29)
+# checks for leap years - years where february has 29 days
+
+the SECOND part is OPTIONAL, and checks for the time and the offset:
+
+(T([0-1]\d|2[0-3]):[0-5]\d(:[0-5]\d((\.\d{0,9})?))?
+# checks if the format is Thh:mm:ss.milliseconds, where the seconds and milliseconds are optional
+# the time after the T can be at to 23:59:59.999999999
+
+([zZ]|((-|\+)(((0\d|1[0-7])(:[0-5]\d)?)|18(:00)?))?))?)$
+# checks for z or Z in the end of the last time part,
+# OR checks if there is an offset in the format +/-hh:mm up to +/-18:00
+
+Extended explanation:
+====================
 (                                      # Start of the pattern
   (                                    # Start of the first group
     (                                  # Start of the first nested group
@@ -42,13 +68,34 @@ Holds a const regular expression to check if datetime is in correct ISO8601 form
     (((0\d|1[0-7])(:[0-5]\d)?)|18(:00)?))? # Matches hours and minutes for time offset
 )?)                                  # End of the pattern
 
+
+Accepted formats and examples:
+================================
+yyyy-mm-dd
+yyyy-mm-ddThh:mm
+yyyy-mm-ddThh:mm. <from 0 to 9 digits after the dot>
+yyyy-mm-ddThh:mm.z
+yyyy-mm-ddThh:mm.Z
+yyyy-mm-ddThh:mm.+hh:mm
+yyyy-mm-ddThh:mm.-hh:mm
+
+2015-08-02
+2015-08-02T19:12
+2020-03-27T03:40:59
+2020-03-27T03:40:59.
+2020-03-27T03:40:59.999999999
+2020-03-27T03:40:59.0001z
+2020-03-27T03:40:59.003Z
+2020-03-27T03:40:59.1+09:58
+2020-03-27T03:40:59.-18:00
+
  */
 @Getter
 public class ISO8601Regex {
      static final public String expression =
              "^(((20([0-9]{2})|2100)-((0[1-9])|(1[0-2]))-(0[1-9]|1\\d|2[0-8])" +
-            "|(20([0-9]{2})|2100)-(((0[13578]|1[02])-31)|((0[13-9]|1[0-2])-(29|30)))|" +
-             "(20(00|04|[2468][048]|[13579][26])|2100)-02-29)" +
+            "|(20([0-9]{2})|2100)-(((0[13578]|1[02])-31)|((0[13-9]|1[0-2])-(29|30)))" +
+             "|(20(00|04|[2468][048]|[13579][26])|2100)-02-29)" +
             "(T([0-1]\\d|2[0-3]):[0-5]\\d(:[0-5]\\d((\\.\\d{0,9})?))?" +
             "([zZ]|((-|\\+)(((0\\d|1[0-7])(:[0-5]\\d)?)|18(:00)?))?))?)$";
 
