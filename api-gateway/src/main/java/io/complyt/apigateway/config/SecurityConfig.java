b0207b0f159127e41e3abf9d1488a4e2694b5906
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-03-29 16:07:48 +0300

    deleted env file

--- api-gateway/src/main/java/io/complyt/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/io/complyt/apigateway/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Profile({"development", "penetration-test", "demo", "test", "default"})
+    @Profile({"development", "demo", "test", "default"})
     @Bean
     public SecurityWebFilterChain nonProductionSecurityWebFilterChain(ServerHttpSecurity http) {
         // CORS
@@ -77,6 +77,7 @@ public class SecurityConfig {
                 .pathMatchers("/actuator/health",
                         "/files/v3/api-docs",
                         "/sales-tax/v3/api-docs",
+                        "/sales-tax-rates/v3/api-docs",
                         "/v3/api-docs/**",
                         "/webjars/**",
                         "/swagger-ui*/**"
