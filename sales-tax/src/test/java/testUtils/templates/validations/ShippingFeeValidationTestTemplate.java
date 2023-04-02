31e8bfd3a121499087c52975245e4c22dea5adf7
Yoav Zinger <yoav@complyt.io>
2023-04-02 18:14:19 +0300

    fix: changed shipping fee taxcode to be able to be blank

--- sales-tax/src/test/java/testUtils/templates/validations/ShippingFeeValidationTestTemplate.java
+++ sales-tax/src/test/java/testUtils/templates/validations/ShippingFeeValidationTestTemplate.java
@@ -8,8 +8,6 @@ public interface ShippingFeeValidationTestTemplate {
 
     void upsert_NullTaxCodeInShippingFee_Returns400ValidationError();
 
-    void upsert_BlankTaxCodeInShippingFee_Returns400ValidationError();
-
     void upsert_LengthGreaterThan256TaxCodeInShippingFee_Returns400ValidationError();
 
 }
