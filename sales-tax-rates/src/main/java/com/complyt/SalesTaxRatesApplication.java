f620d95e293a5be06a73c1487a508e5ee1f0f04a
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-04-13 15:04:07 +0300

    added generated annotation to SalesTaxRatesApplication

--- sales-tax-rates/src/main/java/com/complyt/SalesTaxRatesApplication.java
+++ sales-tax-rates/src/main/java/com/complyt/SalesTaxRatesApplication.java
@@ -1,8 +1,10 @@
 package com.complyt;
 
+import com.complyt.annotations.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Generated
 @SpringBootApplication
 public class SalesTaxRatesApplication {
 
