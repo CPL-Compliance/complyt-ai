4392da7400794de60017b56c5869474aaeb54eb2
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-04-13 14:36:57 +0300

    tests to security package

---
+++ sales-tax-rates/src/test/java/com/example/complyt/security/TenantResolverTest.java
@@ -0,0 +1,36 @@
+package com.example.complyt.security;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+class TenantResolverTest {
+//
+//    @Autowired
+//    TenantResolver tenantResolver;
+//
+//    @MockBean
+//    SecurityContext securityContext;
+//
+//    @Test
+//    void resolve() {
+//        // Given
+//        Jwt jwt = Jwt.withTokenValue(UUID.randomUUID().toString())
+//                .header("Alg", "aaa")
+//                .claim("tenant_id", "tenant")
+//                .build();
+//
+//        JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken(jwt);
+//
+//        // When
+//        when(securityContext.getAuthentication()).thenReturn(jwtAuthenticationToken);
+//        SecurityContextHolder.setContext(securityContext);
+//
+//        Mono<String> tenantMono = tenantResolver.resolve();
+//
+//        // Then
+//        StepVerifier.create(tenantMono).expectNext("tenant").verifyComplete();
+//    }
+}
