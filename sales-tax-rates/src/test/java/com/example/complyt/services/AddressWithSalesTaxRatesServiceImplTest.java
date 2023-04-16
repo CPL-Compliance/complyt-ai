642b39ea8821c64b810c5474eb6e7d2e2abfc0df
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-04-16 14:32:40 +0300

    test to service

--- sales-tax-rates/src/test/java/com/example/complyt/services/AddressWithSalesTaxRatesServiceImplTest.java
+++ sales-tax-rates/src/test/java/com/example/complyt/services/AddressWithSalesTaxRatesServiceImplTest.java
@@ -1,61 +1,68 @@
-//package com.example.complyt.services;
-//
-//import com.complyt.business.mapper.SalesTaxDataToSalesTaxRate;
-//import com.complyt.business.sales_tax_web_clients.SalesTaxWebClientWrapper;
-//import com.complyt.domain.Address;
-//import com.complyt.domain.AddressWithSalesTaxRates;
-//import com.complyt.domain.SalesTaxRates;
-//import com.complyt.domain.StatesMap;
-//import com.complyt.repositories.SalesTaxRatesRepository;
-//import com.complyt.services.SalesTaxRatesServiceImpl;
-//import com.testUtils.TestUtilities;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import reactor.core.publisher.Mono;
-//import reactor.test.StepVerifier;
-//
-//import java.time.LocalDateTime;
-//
-//import static org.mockito.Mockito.when;
-//
-//public class AddressWithSalesTaxRatesServiceImplTest {
-//
-//    @InjectMocks
-//    SalesTaxRatesServiceImpl salesTaxRatesService;
-//
-//    @Mock
-//    SalesTaxRatesRepository salesTaxRatesRepository;
-//
-//    @Mock
-//    SalesTaxWebClientWrapper salesTaxWebClientWrapper;
-//
-//    @Mock
-//    SalesTaxDataToSalesTaxRate salesTaxDataToSalesTaxRate;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//    @Test
-//    void findByAddress_FindsAddressWithRates_ReturnsRates() {
-//        // Given
-//        Address califoniaAddress = TestUtilities.createAddressInCalifornia();
-//        String collectionName = StatesMap.statesToCollections.get(califoniaAddress.getState());
-//        SalesTaxRates californiaRates = TestUtilities.createCaliforniaSalesTaxRates();
-//
-//        AddressWithSalesTaxRates addressWithSalesTaxRates =
-//                new AddressWithSalesTaxRates(califoniaAddress, californiaRates, LocalDateTime.now(), LocalDateTime.now().plusHours(1));
-//
-//        // When
-//        when(salesTaxRatesRepository.findByAddress(califoniaAddress, collectionName)).thenReturn(Mono.just(addressWithSalesTaxRates));
-//        Mono<SalesTaxRates> salesTaxRatesMono = salesTaxRatesService.findByAddress(califoniaAddress);
-//
-//        // Then
-//        StepVerifier.create(salesTaxRatesMono).expectNext(californiaRates).verifyComplete();
-//    }
-//
-//}
+package com.example.complyt.services;
+
+import com.complyt.business.data_fetcher.CountyFetcher;
+import com.complyt.business.mapper.SalesTaxDataToSalesTaxRate;
+import com.complyt.business.sales_tax_web_clients.SalesTaxWebClientWrapper;
+import com.complyt.domain.Address;
+import com.complyt.domain.AddressWithSalesTaxRates;
+import com.complyt.domain.SalesTaxRates;
+import com.complyt.domain.StatesMap;
+import com.complyt.domain.fast_tax.FastTaxData;
+import com.complyt.repositories.AddressWithSalesTaxRatesRepository;
+import com.complyt.services.AddressWithSalesTaxRatesServiceImpl;
+import com.testUtils.TestUtilities;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AddressWithSalesTaxRatesServiceImplTest {
+
+    @InjectMocks
+    AddressWithSalesTaxRatesServiceImpl addressWithSalesTaxRatesService;
+
+    @Mock
+    AddressWithSalesTaxRatesRepository addressWithSalesTaxRatesRepository;
+
+    @Mock
+    SalesTaxWebClientWrapper salesTaxWebClientWrapper;
+
+    @Mock
+    SalesTaxDataToSalesTaxRate salesTaxDataToSalesTaxRate;
+
+    @Mock
+    CountyFetcher countyFetcher;
+
+
+    @Test
+    void findByAddress_FindsAddressWithRates_ReturnsRates() {
+        // Given
+        Address califoniaAddress = TestUtilities.createAddressInCalifornia();
+        String collectionName = StatesMap.statesToCollections.get(califoniaAddress.getState());
+        SalesTaxRates californiaRates = TestUtilities.createCaliforniaSalesTaxRates();
+        FastTaxData fastTaxData = new FastTaxData();
+
+        AddressWithSalesTaxRates expectedAddressWithSalesTaxRates =
+                new AddressWithSalesTaxRates(califoniaAddress, californiaRates, LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+
+        // When
+        when(salesTaxWebClientWrapper.findByAddress(any())).thenReturn(Mono.just(fastTaxData));
+        when(addressWithSalesTaxRatesRepository.findByAddress(califoniaAddress, collectionName)).thenReturn(Mono.just(expectedAddressWithSalesTaxRates));
+        Mono<AddressWithSalesTaxRates> addressWithSalesTaxRatesMono = addressWithSalesTaxRatesService.findByAddress(califoniaAddress);
+
+        // Then
+        StepVerifier.create(addressWithSalesTaxRatesMono).expectNext(expectedAddressWithSalesTaxRates).verifyComplete();
+    }
+
+}
