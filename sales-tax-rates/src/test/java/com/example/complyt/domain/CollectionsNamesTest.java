21e37cbc818c660f53aef3a708c2554397993ab0
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-04-16 14:51:52 +0300

    testsToCollectionsNames

---
+++ sales-tax-rates/src/test/java/com/example/complyt/domain/CollectionsNamesTest.java
@@ -0,0 +1,66 @@
+package com.example.complyt.domain;
+
+import com.complyt.domain.CollectionsNames;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CollectionsNamesTest {
+
+    @Test
+    void testsToCollectionsNames() {
+        CollectionsNames collectionsNames = new CollectionsNames();
+
+        Assertions.assertEquals("alabama",CollectionsNames.ALABAMA);
+        Assertions.assertEquals("alaska",CollectionsNames.ALASKA);
+        Assertions.assertEquals("arizona",CollectionsNames.ARIZONA);
+        Assertions.assertEquals("arkansas",CollectionsNames.ARKANSAS);
+        Assertions.assertEquals("california",CollectionsNames.CALIFORNIA);
+        Assertions.assertEquals("connecticut",CollectionsNames.CONNECTICUT);
+        Assertions.assertEquals("delaware",CollectionsNames.DELAWARE);
+        Assertions.assertEquals("district_of_columbia",CollectionsNames.DISTRICT_OF_COLOMBIA);
+        Assertions.assertEquals("florida",CollectionsNames.FLORIDA);
+        Assertions.assertEquals("georgia",CollectionsNames.GEORGIA);
+        Assertions.assertEquals("hawaii",CollectionsNames.HAWAII);
+        Assertions.assertEquals("idaho",CollectionsNames.IDAHO);
+        Assertions.assertEquals("indiana",CollectionsNames.INDIANA);
+        Assertions.assertEquals("iowa",CollectionsNames.IOWA);
+        Assertions.assertEquals("kansas",CollectionsNames.KANSAS);
+        Assertions.assertEquals("kentucky",CollectionsNames.KENTUCKY);
+        Assertions.assertEquals("louisiana",CollectionsNames.LOUISIANA);
+        Assertions.assertEquals("maine",CollectionsNames.MAINE);
+        Assertions.assertEquals("maryland",CollectionsNames.MARYLAND);
+        Assertions.assertEquals("massachusetts",CollectionsNames.MASSACHUSETTS);
+        Assertions.assertEquals("michigan",CollectionsNames.MICHIGAN);
+        Assertions.assertEquals("minnesota",CollectionsNames.MINNESOTA);
+        Assertions.assertEquals("mississippi",CollectionsNames.MISSISSIPPI);
+        Assertions.assertEquals("missouri",CollectionsNames.MISSOURI);
+        Assertions.assertEquals("montana",CollectionsNames.MONTANA);
+        Assertions.assertEquals("nebraska",CollectionsNames.NEBRASKA);
+        Assertions.assertEquals("nevada",CollectionsNames.NEVADA);
+        Assertions.assertEquals("new_hampshire",CollectionsNames.NEW_HAMPSHIRE);
+        Assertions.assertEquals("new_jersey",CollectionsNames.NEW_JERSEY);
+        Assertions.assertEquals("new_mexico",CollectionsNames.NEW_MEXICO);
+        Assertions.assertEquals("new_york",CollectionsNames.NEW_YORK);
+        Assertions.assertEquals("north_carolina",CollectionsNames.NORTH_CAROLINA);
+        Assertions.assertEquals("north_dakota",CollectionsNames.NORTH_DAKOTA);
+        Assertions.assertEquals("ohio",CollectionsNames.OHIO);
+        Assertions.assertEquals("oklahoma",CollectionsNames.OKLAHOMA);
+        Assertions.assertEquals("oregon",CollectionsNames.OREGON);
+        Assertions.assertEquals("pennsylvania",CollectionsNames.PENNSYLVANIA);
+        Assertions.assertEquals("rhode_island",CollectionsNames.RHODE_ISLAND);
+        Assertions.assertEquals("south_carolina",CollectionsNames.SOUTH_CAROLINA);
+        Assertions.assertEquals("south_dakota",CollectionsNames.SOUTH_DAKOTA);
+        Assertions.assertEquals("tennessee",CollectionsNames.TENNESSEE);
+        Assertions.assertEquals("texas",CollectionsNames.TEXAS);
+        Assertions.assertEquals("utah",CollectionsNames.UTAH);
+        Assertions.assertEquals("vermont",CollectionsNames.VERMONT);
+        Assertions.assertEquals("virginia",CollectionsNames.VIRGINIA);
+        Assertions.assertEquals("washington",CollectionsNames.WASHINGTON);
+        Assertions.assertEquals("west_virginia",CollectionsNames.WEST_VIRGINIA);
+        Assertions.assertEquals("wisconsin",CollectionsNames.WISCONSIN);
+        Assertions.assertEquals("wyoming",CollectionsNames.WYOMING);
+        Assertions.assertEquals("illinois",CollectionsNames.ILLINOIS);
+        Assertions.assertEquals("colorado",CollectionsNames.COLORADO);
+
+    }
+}
