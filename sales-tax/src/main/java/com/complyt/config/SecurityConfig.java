3f7bcb90aa9979cb452f2daba9254275dc010c38
TomerZack <110763328+TomerZack@users.noreply.github.com>
2023-04-17 12:09:09 +0300

    final touches 3

--- sales-tax/src/main/java/com/complyt/config/SecurityConfig.java
+++ sales-tax/src/main/java/com/complyt/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Profile({"development", "penetration-test", "demo", "test", "default", "integration-test"})
+    @Profile({"development", "penetration-test", "demo", "test", "default"})
     @Bean
     public SecurityWebFilterChain nonProductionSecurityWebFilterChain(ServerHttpSecurity http) {
         // CORS
