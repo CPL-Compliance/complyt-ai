7679b7a9c95a92ccf28265511c954eb49cb91dbe
TomerZack <110763328+TomerZack@users.noreply.github.com>
2023-04-17 12:07:57 +0300

    final touches 2

--- sales-tax/src/test/java/integration/scenarios/MultitenancyITTemplate.java
+++ sales-tax/src/test/java/integration/scenarios/MultitenancyITTemplate.java
@@ -3,12 +3,20 @@ package integration.scenarios;
 public interface MultitenancyITTemplate {
 
     void getCustomer_ExistsInOtherTenant_Returns404();
+
     void getTransaction_ExistsInOtherTenant_Returns404();
+
     void getSalesTaxTracking_ExistsInOtherTenant_Returns404();
+
     void putCustomer_WithComplytIdAndExistsInOtherTenant_Returns400DataConflict();
+
     void putTransaction_WithComplytIdAndExistsInOtherTenant_Returns400DataConflict();
+
     void putSalesTaxTracking_WithComplytIdAndExistsInOtherTenant_Returns400DataConflict();
+
     void putCustomer_ExistsInOtherTenant_Returns200WithoutDataLeak();
+
     void putTransaction_ExistsInOtherTenant_Returns200WithoutDataLeak();
+
     void putSalesTaxTracking_ExistsInOtherTenant_Returns200WithoutDataLeak();
 }
