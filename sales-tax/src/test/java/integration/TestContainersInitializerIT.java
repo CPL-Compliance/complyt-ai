c1ac71da2dce9dfd4b6cc118cee22d0d0d0ffa46
TomerZack <110763328+TomerZack@users.noreply.github.com>
2023-04-18 14:44:22 +0300

    temp

--- sales-tax/src/test/java/integration/TestContainersInitializerIT.java
+++ sales-tax/src/test/java/integration/TestContainersInitializerIT.java
@@ -3,8 +3,10 @@ package integration;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.utility.DockerImageName;
 
 @Slf4j
@@ -17,6 +19,16 @@ public abstract class TestContainersInitializerIT {
     protected static final MongoDBContainer MONGO_CONTAINER = new MongoDBContainer(DockerImageName.parse(MONGO_IMAGE))
             .withExposedPorts(27017)
             .withClasspathResourceMapping("sales_tax.dump", "sales_tax.dump", BindMode.READ_ONLY);
+
+    protected static GenericContainer DISCOVERY_CONTAINER = new GenericContainer<>( new ImageFromDockerfile()
+            .withDockerfileFromBuilder(builder -> builder
+                            .from("amazoncorretto:17-al2023-jdk")
+                            .add("maven/discovery-service-0.11.0-SNAPSHOT.jar", "app.jar")
+                            .run("yum install -y wget")
+                            .run("wget -O dd-java-agent.jar 'https://dtdg.co/latest-java-tracer'")
+                            .run("sh -c 'touch app.jar'")
+                            .entryPoint("java","-Djava.security.egd=file:/dev/./urandom","-jar","app.jar")
+                    ));
 
     static {
         MONGO_CONTAINER.start();
