021d43a4fafb86c2e81b1a35ccff8b5eb29dbaf4
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-05-18 14:48:56 +0300

    added route to rates swagger in gateway dev

--- sales-tax/src/main/java/com/complyt/business/transaction/data_fetcher/TransactionCountyFetcher.java
+++ sales-tax/src/main/java/com/complyt/business/transaction/data_fetcher/TransactionCountyFetcher.java
@@ -6,7 +6,6 @@ import com.complyt.domain.sales_tax.ComplytSalesTaxRates;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
