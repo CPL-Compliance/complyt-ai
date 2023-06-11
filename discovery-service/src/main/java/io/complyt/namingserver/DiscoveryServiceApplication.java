22b13730b934350f640bddba43100b9a94184c33
Alexander Tilkin <alextilk@gmail.com>
2023-06-11 11:54:46 +0300

    WIP

--- discovery-service/src/main/java/io/complyt/namingserver/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/io/complyt/namingserver/DiscoveryServiceApplication.java
@@ -1,9 +1,15 @@
 package io.complyt.namingserver;
 
 import io.complyt.namingserver.annotations.Generated;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.BeanProperty;
 
 @Generated
 @EnableEurekaServer
@@ -13,5 +19,4 @@ public class DiscoveryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
-
 }
