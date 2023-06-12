a5d027730d08ecb13b61cfb408981bfa7b10f5f3
Alexander Tilkin <alextilk@gmail.com>
2023-06-12 10:53:02 +0300

    I modified the profiles for the test env to use IPs

--- discovery-service/src/main/java/io/complyt/namingserver/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/io/complyt/namingserver/DiscoveryServiceApplication.java
@@ -1,15 +1,9 @@
 package io.complyt.namingserver;
 
 import io.complyt.namingserver.annotations.Generated;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.beans.BeanProperty;
 
 @Generated
 @EnableEurekaServer
