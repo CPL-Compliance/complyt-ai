6204c5033c71d31a719bee35dfb0d1659418254b
Alexander Tilkin <alextilk@gmail.com>
2023-06-15 16:09:31 +0300

    I'm trying to apply replica set with ip and port

--- discovery-service/src/main/java/io/complyt/namingserver/config/EurekaConfig.java
+++ discovery-service/src/main/java/io/complyt/namingserver/config/EurekaConfig.java
@@ -2,21 +2,35 @@ package io.complyt.namingserver.config;
 
 import com.netflix.appinfo.AmazonInfo;
 import io.complyt.namingserver.annotations.Generated;
+import lombok.NonNull;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Generated
 @Configuration
 public class EurekaConfig {
+    @NonNull
+    Environment environment;
+
     @Bean
-    @Profile("production")
-    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+    @Profile("production, az-1a, az-1b")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) throws UnknownHostException {
         EurekaInstanceConfigBean bean = new EurekaInstanceConfigBean(inetUtils);
         AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
         bean.setDataCenterInfo(info);
+
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = environment.getProperty("server.port");
+        bean.setNonSecurePort(Integer.parseInt(port));
+        bean.setIpAddress(ip);
+        bean.setPreferIpAddress(true);
 
         return bean;
     }
