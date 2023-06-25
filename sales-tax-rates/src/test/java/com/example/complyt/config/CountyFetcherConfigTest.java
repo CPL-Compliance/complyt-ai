86ad5ab32a9c16d7a4193c64645e7d0983c187ce
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-06-25 14:42:35 +0300

    CountyFetcherConfigTest

--- sales-tax-rates/src/test/java/com/example/complyt/config/CountyFetcherConfigTest.java
+++ sales-tax-rates/src/test/java/com/example/complyt/config/CountyFetcherConfigTest.java
@@ -1,6 +1,7 @@
 package com.example.complyt.config;
 
 import com.complyt.business.data_fetcher.FastTaxCountyFetcher;
+import com.complyt.business.data_fetcher.TaxJarCountyFetcher;
 import com.complyt.business.data_fetcher.ZipTaxCountyFetcher;
 import com.complyt.config.CountyFetcherConfig;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +19,7 @@ public class CountyFetcherConfigTest {
     }
 
     @Test
-    void transactionFastTaxCountyFetcher_SalesTaxWebClientWrapper_ReturnedTransactionFastTaxCountyFetcher() {
+    void fastTaxCountyFetcher_SalesTaxWebClientWrapper_ReturnedTransactionFastTaxCountyFetcher() {
         // Given
         FastTaxCountyFetcher expectedFastTaxCountyFetcher = new FastTaxCountyFetcher();
 
@@ -30,7 +31,7 @@ public class CountyFetcherConfigTest {
     }
 
     @Test
-    void transactionZipTaxCountyFetcher_SalesTaxWebClientWrapper_ReturnedTransactionZipTaxCountyFetcher() {
+    void zipTaxCountyFetcher_SalesTaxWebClientWrapper_ReturnedTransactionZipTaxCountyFetcher() {
         // Given
         ZipTaxCountyFetcher expectedZipTaxCountyFetcher = new ZipTaxCountyFetcher();
 
@@ -39,6 +40,18 @@ public class CountyFetcherConfigTest {
 
         // Then
         assertEquals(expectedZipTaxCountyFetcher, actualZipTaxCountyFetcher);
+    }
+
+    @Test
+    void taxJarCountyFetcher_SalesTaxWebClientWrapper_ReturnedTransactionZipTaxCountyFetcher() {
+        // Given
+        TaxJarCountyFetcher expectedTaxJarCountyFetcher = new TaxJarCountyFetcher();
+
+        // When
+        TaxJarCountyFetcher actualTaxJarCountyFetcher = countyFetcherConfig.taxJarCountyFetcher();
+
+        // Then
+        assertEquals(expectedTaxJarCountyFetcher, actualTaxJarCountyFetcher);
     }
 
 }
