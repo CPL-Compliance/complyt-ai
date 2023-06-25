763e0b7a8e2ee5fb3cbf050eaf2ceb0162d081df
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-06-25 13:22:07 +0300

    changed parameters to be sent as blank in case being received as null

--- sales-tax-rates/src/main/java/com/complyt/business/sales_tax_web_clients/TaxJarWebClientWrapper.java
+++ sales-tax-rates/src/main/java/com/complyt/business/sales_tax_web_clients/TaxJarWebClientWrapper.java
@@ -19,12 +19,16 @@ public final class TaxJarWebClientWrapper extends SalesTaxWebClientWrapperBase {
         this.client = client;
     }
 
+    /*
+        We will send city and street as blank in case they are being received as null
+        because taxJar api doesn't accept null values as address parameters
+     */
     public Mono<SalesTaxData> findByAddress(String zip, String address, String city, String country) {
         try {
             Map<String, String> params = new HashMap<>();
-            params.put("country", country);
-            params.put("city", city);
-            params.put("street", address);
+            params.put("country", country == null ? "US" : country);
+            params.put("city", city == null ? "" : city);
+            params.put("street", address == null ? "" : address);
             RateResponse res = client.ratesForLocation(zip, params);
             TaxJarData taxJarData = new TaxJarData(res.rate);
             return Mono.just(taxJarData);
