11a287a1bf4d14ae2fbc08adc6b541c7297b8d28
Alexander Tilkin <alextilk@gmail.com>
2023-06-27 17:29:14 +0300

    Restoring old files

---
+++ discovery-service/src/main/java/io/complyt/namingserver/config/EurekaConfig.java
@@ -0,0 +1,23 @@
+package io.complyt.namingserver.config;
+
+import com.netflix.appinfo.AmazonInfo;
+import io.complyt.namingserver.annotations.Generated;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Generated
+@Configuration
+public class EurekaConfig {
+    @Bean
+    @Profile({"production", "az-1a", "az-1b"})
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean bean = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        bean.setDataCenterInfo(info);
+
+        return bean;
+    }
+}
