b9684bfabd9bc5e178504b72b8e98fa584b89f1f
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-06-29 12:12:44 +0300

    edited SalesTaxRates record

--- sales-tax-rates/src/main/java/com/complyt/domain/SalesTaxRates.java
+++ sales-tax-rates/src/main/java/com/complyt/domain/SalesTaxRates.java
@@ -3,8 +3,9 @@ package com.complyt.domain;
 import lombok.With;
 
 @With
-public record SalesTaxRates(float cityRate, float countyRate, float stateRate, float taxRate, float combinedDistrictRate, RatesMetaData ratesMetaData) {
+public record SalesTaxRates(float cityRate, float countyRate, float stateRate, float taxRate,
+                            float combinedDistrictRate, RatesMetaData ratesMetaData) {
     public static SalesTaxRates zeroSalesTaxRates() {
-        return new SalesTaxRates(0, 0, 0, 0, 0, null);
+        return new SalesTaxRates(0, 0, 0, 0, 0, new RatesMetaData(0, 0));
     }
 }
--- sales-tax/src/main/java/com/complyt/domain/sales_tax/SalesTaxRates.java
+++ sales-tax/src/main/java/com/complyt/domain/sales_tax/SalesTaxRates.java
@@ -5,7 +5,8 @@ import lombok.With;
 @With
 public record SalesTaxRates(float cityRate, float countyRate, float stateRate, float taxRate,
                             float combinedDistrictRate, RatesMetaData ratesMetaData) {
+
     public static SalesTaxRates zeroSalesTaxRate() {
-        return new SalesTaxRates(0, 0, 0, 0, 0, null);
+        return new SalesTaxRates(0, 0, 0, 0, 0, new RatesMetaData(0, 0));
     }
 }
