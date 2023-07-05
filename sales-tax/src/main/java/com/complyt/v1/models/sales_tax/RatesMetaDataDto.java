fd6012cd6f86462c7f142764cef57053fe987d29
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-07-05 11:37:13 +0300

    i moved taxjars version to parent pom

--- sales-tax/src/main/java/com/complyt/v1/models/sales_tax/RatesMetaDataDto.java
+++ sales-tax/src/main/java/com/complyt/v1/models/sales_tax/RatesMetaDataDto.java
@@ -1,9 +1,7 @@
 package com.complyt.v1.models.sales_tax;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.With;
 
-@With
 @Schema(name = "RatesMetaData")
 public record RatesMetaDataDto(float cityDistrictRate, float countyDistrictRate) {
 }
