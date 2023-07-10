723c898b88802a25acc612f23648720821d05ae8
TomerZack <110763328+TomerZack@users.noreply.github.com>
2023-07-10 18:32:13 +0300

    test10

--- sales-tax-rates/src/main/java/com/complyt/SalesTaxRatesApplication.java
+++ sales-tax-rates/src/main/java/com/complyt/SalesTaxRatesApplication.java
@@ -11,6 +11,6 @@ public class SalesTaxRatesApplication {
     public static void main(String[] args) {
         SpringApplication.run(SalesTaxRatesApplication.class, args);
 
-        
+        System.out.println("sdgsdgsdgadsg");
     }
 }
