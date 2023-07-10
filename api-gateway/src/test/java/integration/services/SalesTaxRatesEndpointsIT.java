b2a50002254d0f06ae704aea587e45e2e805b783
TomerZack <110763328+TomerZack@users.noreply.github.com>
2023-07-10 18:22:08 +0300

    test5

--- api-gateway/src/test/java/integration/services/SalesTaxRatesEndpointsIT.java
+++ api-gateway/src/test/java/integration/services/SalesTaxRatesEndpointsIT.java
@@ -60,4 +60,29 @@ public class SalesTaxRatesEndpointsIT extends TestContainersInitializerIT implem
                 .jsonPath("$.address.county").isEqualTo("Arapahoe")
                 .jsonPath("$.salesTaxRates.taxRate").isEqualTo(0.04f);
     }
+
+    @Order(2)
+    @Test
+    @Override
+    public void findByAddress_SecondAddressToInsert_InsertsNewComplytSalesTaxRatesAndReturnsIt() {
+        WEB_TEST_CLIENT
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(TestUtilities.SALES_TAX_RATES_BASE_URL)
+                        .queryParam("country", "US")
+                        .queryParam("state", "NY")
+                        .queryParam("city", "cityOfSecondAddress")
+                        .queryParam("street", "160 Broadway")
+                        .queryParam("zip", "10038")
+                        .build())
+                .headers(headers -> {
+                    headers.setBearerAuth(TOKEN);
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                })
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.address.county").isEqualTo("Arapahoe")
+                .jsonPath("$.salesTaxRates.taxRate").isEqualTo(0.04f);
+    }
 }
