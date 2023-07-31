6b881bbb001f391b3231d5d5b50168a6c76d7670
TomerZack <110763328+TomerZack@users.noreply.github.com>
2023-07-31 11:33:58 +0300

    minimized

--- sales-tax-rates/src/main/java/com/complyt/v1/config/error_messages/GenericErrorMessages.java
+++ sales-tax-rates/src/main/java/com/complyt/v1/config/error_messages/GenericErrorMessages.java
@@ -2,7 +2,6 @@ package com.complyt.v1.config.error_messages;
 
 public interface GenericErrorMessages {
 
-    String DATA_CONFLICT_ERROR = "The requested operation failed because there was an unresolvable conflict between two or more inputs";
     String INTERNAL_SERVER_ERROR = "The request failed due to an internal error. Please contact support@complyt.io if this continues";
     String NOT_FOUND_ERROR = "The requested operation failed because a resource associated with the request could not be found.";
 
--- sales-tax-rates/src/main/java/com/complyt/v1/config/error_messages/StringErrorMessages.java
+++ sales-tax-rates/src/main/java/com/complyt/v1/config/error_messages/StringErrorMessages.java
@@ -2,16 +2,7 @@ package com.complyt.v1.config.error_messages;
 
 public interface StringErrorMessages {
 
-    String MINMAX_200_ERROR = "should be 1-200 characters maximum";
-
-    String MINMAX_20_ERROR = "should be 1-20 characters maximum";
-
-    String MINMAX_100_ERROR = "should be 1-100 characters maximum";
-
     String MAX_100_ERROR = "should be 100 characters maximum";
-
-    String MINMAX_50_ERROR = "should be 1-50 characters maximum";
-
     String MAX_50_ERROR = "should be up to 50 characters maximum";
     String MAX_200_ERROR = "should be up to 200 characters maximum";
     String MAX_20_ERROR = "should be up to 20 characters maximum";
