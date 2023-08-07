180cd62a90ed87425dd0315f5f8dc31e361ee425
Alexander Tilkin <alextilk@gmail.com>
2023-08-07 14:33:16 +0300

    I finished create secret key api [skip ci]

---
+++ authentication/src/main/java/io/complyt/authentication/v1/routers/SecretKeyRouter.java
@@ -0,0 +1,26 @@
+package io.complyt.authentication.v1.routers;
+
+import io.complyt.authentication.v1.api_info.PostApiKeyApiInfo;
+import io.complyt.authentication.v1.handlers.SecretKeyHandler;
+import io.complyt.authentication.v1.handlers.TokenHandler;
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+
+@Configuration
+public class SecretKeyRouter {
+    public static final String BASE_URL = "/v1/secret_key";
+
+    @Bean
+    @PostApiKeyApiInfo
+    public RouterFunction<ServerResponse> getSecretKeyRouterFunction(@NonNull final SecretKeyHandler secretKeyHandler) {
+        RequestPredicate getSecretKeyRoute = RequestPredicates
+                .GET(BASE_URL)
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON));
+
+        return RouterFunctions.route(getSecretKeyRoute, secretKeyHandler::get);
+    }
+}
