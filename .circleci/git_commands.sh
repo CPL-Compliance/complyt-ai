e43f0603f4e1a403240116c2038ea1c35d632f8e
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-08-09 18:35:25 +0300

    diff

--- .circleci/git_commands.sh
+++ .circleci/git_commands.sh
@@ -1,9 +1,9 @@
-git checkout eyal/com-235-api-services-versioning-management
-git pull origin eyal/com-235-api-services-versioning-management
+git checkout main
+git pull origin main
 service_name="$1"
 service_version="$2"
 branch_name="$service_name-$service_version"
 git merge --no-edit "$branch_name"
 git commit --amend -m "[skip ci]"
-git push --set-upstream origin eyal/com-235-api-services-versioning-management
+git push --set-upstream origin main
 git push --delete origin "$branch_name"
