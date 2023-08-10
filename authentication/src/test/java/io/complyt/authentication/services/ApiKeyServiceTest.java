802faeaaa3248c4b548e118c6caf0c698ce45702
Alexander Tilkin <alextilk@gmail.com>
2023-08-10 14:33:58 +0300

    UTs WIP

---
+++ authentication/src/test/java/io/complyt/authentication/services/ApiKeyServiceTest.java
@@ -0,0 +1,49 @@
+package io.complyt.authentication.services;
+
+import io.complyt.authentication.security.AesSecretKeyUtils;
+import io.complyt.authentication.security.ApiKeyGenerator;
+import io.complyt.authentication.v1.models.ApiKey;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import testUtils.TestUtilities;
+
+import javax.crypto.KeyGenerator;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+class ApiKeyServiceTest {
+
+    @InjectMocks
+    ApiKeyService apiKeyService;
+
+    @Test
+    void generate_returnsApiKeyInStringFormat() {
+        try (MockedStatic<ApiKeyGenerator> utilities = Mockito.mockStatic(ApiKeyGenerator.class)) {
+            String apiKey = TestUtilities.apiKeyStr;
+            utilities.when(ApiKeyGenerator::generate).thenReturn(apiKey);
+            assertEquals(apiKey, apiKeyService.generate());
+        }
+    }
+
+    @Test
+    void generatefromString_returnApiKey() {
+        // Given
+        String apiKeyStr = TestUtilities.apiKeyStr;
+        ApiKey expectedApiKey = new ApiKey(apiKeyStr);
+
+        ApiKey actualApiKey = apiKeyService.generatefromString(apiKeyStr);
+
+        assertEquals(expectedApiKey, actualApiKey);
+    }
+}
