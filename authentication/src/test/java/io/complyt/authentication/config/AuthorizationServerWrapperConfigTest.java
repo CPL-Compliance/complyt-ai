0222e9977c8fd4218a9be0965c6e264cc9453a54
Alexander Tilkin <alextilk@gmail.com>
2023-08-29 20:23:26 +0300

    IT WIP

--- authentication/src/test/java/io/complyt/authentication/config/AuthorizationServerWrapperConfigTest.java
+++ authentication/src/test/java/io/complyt/authentication/config/AuthorizationServerWrapperConfigTest.java
@@ -1,14 +1,18 @@
 package io.complyt.authentication.config;
 
+import io.complyt.authentication.business.authorization.AccessToken;
 import io.complyt.authentication.business.authorization.Auth0AuthorizationServerWrapper;
+import io.complyt.authentication.business.authorization.StubAuth0AuthorizationServerWrapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.reactive.function.client.WebClient;
+import testUtils.TestUtilities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
@@ -30,4 +34,13 @@ class AuthorizationServerWrapperConfigTest {
         assertEquals(expectedAuth0AuthorizationServerWrapper, actualAuth0AuthorizationServerWrapper);
     }
 
+    @Test
+    void authorizationServerWrapper_webClientIsNull_throwNullException() {
+        // When
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+            authorizationServerWrapperConfig.auth0AuthorizationServerWrapper(null);
+        });
+
+        assertEquals("webClient is marked non-null but is null", exception.getMessage());
+    }
 }
