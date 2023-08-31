3af46bb2698d7c28fc0bab5e73699a4b043b4114
Alexander Tilkin <alextilk@gmail.com>
2023-08-31 15:21:55 +0300

    UTs WIP

--- authentication/src/main/java/io/complyt/authentication/v1/models/properties/ComplytIdPropertyDto.java
+++
@@ -1,8 +0,0 @@
-package io.complyt.authentication.v1.models.properties;
-
-import java.util.UUID;
-
-public interface ComplytIdPropertyDto {
-    UUID complytId();
-    ComplytIdPropertyDto withComplytId(UUID complytId);
-}
