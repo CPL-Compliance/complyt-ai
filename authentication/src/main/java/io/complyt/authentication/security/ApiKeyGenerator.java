0eaa7ca4754252672eb02b106e4d2f848d1de78d
Alexander Tilkin <alextilk@gmail.com>
2023-10-15 10:24:35 +0300

    NonNull added to ApiKey generator

--- authentication/src/main/java/io/complyt/authentication/security/ApiKeyGenerator.java
+++ authentication/src/main/java/io/complyt/authentication/security/ApiKeyGenerator.java
@@ -1,5 +1,6 @@
 package io.complyt.authentication.security;
 
+import lombok.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -7,7 +8,7 @@ import java.util.UUID;
 @Component
 public class ApiKeyGenerator {
 
-    public static String generate() {
+    public static @NonNull String generate() {
         UUID clientId = UUID.randomUUID();
         UUID clientSecret = UUID.randomUUID();
         String delimiter = "-";
