fd94520d9dc095cd3c708df0f9aaa8de2670340b
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-10-17 13:22:18 +0300

    AddressQueryBuilderTest

---
+++ sales-tax-rates/src/test/java/com/example/complyt/repositores/AddressQueryBuilderTest.java
@@ -0,0 +1,74 @@
+package com.example.complyt.repositores;
+
+import com.complyt.domain.Address;
+import com.complyt.repositories.AddressQueryBuilder;
+import com.complyt.repositories.QueryBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import testUtils.TestUtilities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AddressQueryBuilderTest {
+
+    QueryBuilder<Address> addressQueryBuilder;
+
+    Address fullAddress;
+
+    Address partialAddress;
+
+    @BeforeEach
+    void setUp() {
+        addressQueryBuilder = new AddressQueryBuilder();
+        fullAddress = TestUtilities.createAddressInCalifornia();
+        partialAddress = fullAddress
+                .withPartial(true)
+                .withCity(null)
+                .withStreet(null);
+    }
+
+    @Test
+    void build_PartialAddressPassed_ReturnsPartialAddressQuery() {
+        // Given
+        Query expectedQuery = Query.query(Criteria.where("address.zip").is(partialAddress.zip()));
+
+        // When
+        Query actualQuery = addressQueryBuilder.build(partialAddress);
+
+        // Then
+        Assertions.assertEquals(expectedQuery, actualQuery);
+    }
+
+    @Test
+    void build_FullyAddressPassed_ReturnsFullyAddressQuery() {
+        // Given
+        Query expectedQuery = Query.query(Criteria
+                .where("address.city").is(fullAddress.city())
+                .and("address.street").is(fullAddress.street())
+                .and("address.zip").is(fullAddress.zip()));
+
+        // When
+        Query actualQuery = addressQueryBuilder.build(fullAddress);
+
+        // Then
+        Assertions.assertEquals(expectedQuery, actualQuery);
+    }
+
+    @Test
+    void build_NullAddressPassed_ThrowsException() {
+        // Given
+        Address nullAddress = null;
+
+        // When + Then
+        NullPointerException nullPointerException = assertThrows(NullPointerException.class, () -> {
+            addressQueryBuilder.build(nullAddress);
+        });
+
+        assertEquals(nullPointerException.getMessage(), "address " + TestUtilities.LOMBOK_NON_NULL_ANNOTATION_MESSAGE);
+    }
+
+}
