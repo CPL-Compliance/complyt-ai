958e8203ea457005591f245dad5013500d740b0b
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2023-10-25 20:40:30 +0300

    modified bean scan for mapper in rates

--- sales-tax-rates/src/main/java/com/complyt/config/MappersConfig.java
+++ sales-tax-rates/src/main/java/com/complyt/config/MappersConfig.java
@@ -6,33 +6,32 @@ import com.complyt.domain.mappers.TaxJarDataToSalesTaxRateMapper;
 import com.complyt.domain.mappers.ZipTaxDataToSalesTaxRateMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MappersConfig {
 
-    @Primary
-    @Profile({"fastTax", "stubFastTax", "default"})
-    @Bean("salesTaxDataToSalesTaxRateMapper")
-    public FastTaxGetBestMatchDataToSalesTaxRateMapper fastTaxGetBestMatchDataGetBestMatchToSalesTaxRateMapper() {
-        return FastTaxGetBestMatchDataToSalesTaxRateMapper.INSTANCE;
-    }
-
     @Profile({"fastTax"})
-    @Bean("salesTaxDataToSalesTaxRateMapper")
+    @Bean("getByCityCountyDataToSalesTaxRateMapper")
     public FastTaxGetByCityCountyStateDataToSalesTaxRateMapper fastTaxGetByCityCountyDataToSalesTaxRateMapper() {
         return FastTaxGetByCityCountyStateDataToSalesTaxRateMapper.INSTANCE;
     }
 
+    @Profile({"fastTax", "stubFastTax", "default"})
+    @Bean("getBestMatchDataToSalesTaxRateMapper")
+    public FastTaxGetBestMatchDataToSalesTaxRateMapper fastTaxGetBestMatchDataGetBestMatchToSalesTaxRateMapper() {
+        return FastTaxGetBestMatchDataToSalesTaxRateMapper.INSTANCE;
+    }
+
+
     @Profile({"zipTax"})
-    @Bean("salesTaxDataToSalesTaxRateMapper")
+    @Bean("getBestMatchDataToSalesTaxRateMapper")
     public ZipTaxDataToSalesTaxRateMapper zipTaxDataToSalesTaxRateMapper() {
         return ZipTaxDataToSalesTaxRateMapper.INSTANCE;
     }
 
     @Profile({"taxJar"})
-    @Bean("salesTaxDataToSalesTaxRateMapper")
+    @Bean("getBestMatchDataToSalesTaxRateMapper")
     public TaxJarDataToSalesTaxRateMapper taxJarDataToSalesTaxRateMapper() {
         return TaxJarDataToSalesTaxRateMapper.INSTANCE;
     }
