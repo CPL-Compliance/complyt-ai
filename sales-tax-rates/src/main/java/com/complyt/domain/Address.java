63ae5d8be5c83cf80f2f5a733b34a1ac8bcfc076
Alexander Tilkin <alextilk@gmail.com>
2023-12-06 10:05:19 +0200

    First commit

--- sales-tax-rates/src/main/java/com/complyt/domain/Address.java
+++ sales-tax-rates/src/main/java/com/complyt/domain/Address.java
@@ -1,7 +1,9 @@
 package com.complyt.domain;
 
+import lombok.Builder;
 import lombok.With;
 
+@Builder
 @With
 public record Address(String city, String country, String county, String state, String street, String zip,
                       boolean isPartial) {
