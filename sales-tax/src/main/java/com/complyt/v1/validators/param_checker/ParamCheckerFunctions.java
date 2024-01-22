ab9adeb6810f2e9cac73f6e3e1f08cf422040b50
RazAdani <raz@complyt.io>
2024-01-22 15:29:00 +0200

    change date regex

--- sales-tax/src/main/java/com/complyt/v1/validators/param_checker/ParamCheckerFunctions.java
+++ sales-tax/src/main/java/com/complyt/v1/validators/param_checker/ParamCheckerFunctions.java
@@ -13,6 +13,6 @@ public interface ParamCheckerFunctions  {
     Function<String, Mono<String>> SIZE_CHECK = ParameterCheckableDefault.createParamCheckerFunction(NumericRegex.expression, DtoErrorMessages.SIZE_FORMAT_ERROR);
     Function<String, Mono<String>> EXTERNAL_ID_NOT_NULL_CHECK = ParameterCheckableDefault.createParamCheckerFunction(NotNullUndefinedRegex.expression, DtoErrorMessages.EXTERNAL_ID_NOT_NULL_ERROR);
     Function<String, Mono<String>> STATE_CHECK = ParameterCheckableDefault.createParamCheckerFunction(StateRegex.expression, DtoErrorMessages.STATE_FORMAT_ERROR);
-    Function<String, Mono<String>> DATE_CHECK = ParameterCheckableDefault.createParamCheckerFunction(LocalDateRegex.expression, "date " + DtoErrorMessages.LOCALDATE_FORMAT_ERROR);
+    Function<String, Mono<String>> DATE_CHECK = ParameterCheckableDefault.createParamCheckerFunction(ISO8601Regex.expression, "date " + DtoErrorMessages.LOCALDATE_FORMAT_ERROR);
 
 }
