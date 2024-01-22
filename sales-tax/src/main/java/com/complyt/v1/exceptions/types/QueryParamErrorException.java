8cc778dbbfe4c9a8d46ffcff5cd58823391fef84
RazAdani <raz@complyt.io>
2024-01-22 14:52:26 +0200

    added generated for QueryParamException

--- sales-tax/src/main/java/com/complyt/v1/exceptions/types/QueryParamErrorException.java
+++ sales-tax/src/main/java/com/complyt/v1/exceptions/types/QueryParamErrorException.java
@@ -1,9 +1,11 @@
 package com.complyt.v1.exceptions.types;
 
+import com.complyt.annotations.Generated;
 import com.complyt.v1.config.error_messages.GenericErrorMessages;
 
 import java.util.List;
 
+@Generated
 public class QueryParamErrorException extends ComplytApiException {
 
     public QueryParamErrorException(List<String> errorList) {
