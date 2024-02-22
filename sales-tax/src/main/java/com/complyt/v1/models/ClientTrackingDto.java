847063e1302f06475bd39629f248058119d57e7c
RazAdani <raz@complyt.io>
2024-02-22 17:01:30 +0200

    adding message errors

--- sales-tax/src/main/java/com/complyt/v1/models/ClientTrackingDto.java
+++ sales-tax/src/main/java/com/complyt/v1/models/ClientTrackingDto.java
@@ -1,5 +1,6 @@
 package com.complyt.v1.models;
 
+import com.complyt.v1.config.error_messages.DtoErrorMessages;
 import com.complyt.v1.config.error_messages.StringErrorMessages;
 import com.complyt.v1.models.nexus.NexusDto;
 import jakarta.validation.Valid;
@@ -9,8 +10,8 @@ import lombok.With;
 
 @With
 public record ClientTrackingDto (
-    @Valid @NotNull NexusDto nexus,
-    @NotNull @Size(max = 256, message = "ClientTracking.name " + StringErrorMessages.MAX_256_ERROR) String name,
+    @Valid @NotNull(message = "ClientTracking.nexus " + DtoErrorMessages.NOT_NULL_ERROR) NexusDto nexus,
+    @NotNull(message = "ClientTracking.name " + DtoErrorMessages.NOT_NULL_ERROR) @Size(max = 256, message = "ClientTracking.name " + StringErrorMessages.MAX_256_ERROR) String name,
 
     @Valid TimestampsDto internalTimestamps)
 {
