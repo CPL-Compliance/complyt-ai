7fde3b44107302f15a75a0e382e2c657cb00764c
RazAdani <raz@complyt.io>
2024-04-25 17:21:38 +0300

    add testing for authentication schemas

---
+++ authentication/src/test/java/integration/mongo_validation/CredentialsSchemaValidationIT.java
@@ -0,0 +1,263 @@
+package integration.mongo_validation;
+
+import integration.SecurityConfig;
+import integration.TestContainersInitializerIT;
+import io.complyt.authentication.AuthenticationApplication;
+import org.bson.Document;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.test.StepVerifier;
+import test_utils.unit_tests.TestUtilities;
+
+import java.util.Date;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = AuthenticationApplication.class)
+@AutoConfigureWebTestClient
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ContextConfiguration(classes = {
+        SecurityConfig.class})
+public class CredentialsSchemaValidationIT extends TestContainersInitializerIT {
+
+    @Autowired
+    ReactiveMongoTemplate reactiveMongoTemplate;
+
+    Document credentials;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", () -> MONGO_CONTAINER.getReplicaSetUrl("authentication"));
+    }
+
+    @BeforeEach
+    void setup() {
+        credentials = TestUtilities.credentialsDocument();;
+    }
+
+    @Test
+    public void saveCredentials_validCredentials_Success() {
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    public void saveCredentials_MissingComplytClientId_throwsValidationError() {
+        credentials.remove("complytClientId");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingComplytClientSecret_throwsValidationError() {
+        credentials.remove("complytClientSecret");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingClientId_throwsValidationError() {
+        credentials.remove("clientId");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingClientSecret_throwsValidationError() {
+        credentials.remove("clientSecret");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingClientIdIv_throwsValidationError() {
+        credentials.remove("clientIdIv");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingClientSecretIv_throwsValidationError() {
+        credentials.remove("clientSecretIv");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingAudience_throwsValidationError() {
+        credentials.remove("audience");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingGrantType_throwsValidationError() {
+        credentials.remove("grantType");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingTenantId_throwsValidationError() {
+        credentials.remove("tenantId");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingName_throwsValidationError() {
+        credentials.remove("name");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_MissingStatus_throwsValidationError() {
+        credentials.remove("status");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidTenantIdType_throwsValidationError() {
+        credentials.put("tenantId", 123);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidNameType_throwsValidationError() {
+        credentials.put("name", 456);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidStatusType_throwsValidationError() {
+        credentials.put("status", new Object());
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidClientIdType_throwsValidationError() {
+        credentials.put("clientId", false);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidClientSecretIvType_throwsValidationError() {
+        credentials.put("clientSecretIv", 123);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidAudienceType_throwsValidationError() {
+        credentials.put("audience", 789);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidGrantType_throwsValidationError() {
+        credentials.put("grantType", new Date());
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidClientSecretType_throwsValidationError() {
+        credentials.put("clientSecret", 123);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidComplytClientIdType_throwsValidationError() {
+        credentials.put("complytClientId", new Date());
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidComplytClientSecretType_throwsValidationError() {
+        credentials.put("complytClientSecret", true);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidClientIdIvType_throwsValidationError() {
+        credentials.put("clientIdIv", 456);
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveCredentials_InvalidExpireAtType_throwsValidationError() {
+        credentials.put("expireAt", "invalidDate");
+
+        StepVerifier.create(reactiveMongoTemplate.save(credentials, "credentials"))
+                .expectError()
+                .verify();
+    }
+}
---
+++ authentication/src/test/java/integration/mongo_validation/TokenSchemaValidationIT.java
@@ -0,0 +1,221 @@
+package integration.mongo_validation;
+
+import integration.SecurityConfig;
+import integration.TestContainersInitializerIT;
+import io.complyt.authentication.AuthenticationApplication;
+import org.bson.Document;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.test.StepVerifier;
+import test_utils.unit_tests.TestUtilities;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = AuthenticationApplication.class)
+@AutoConfigureWebTestClient
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ContextConfiguration(classes = {
+        SecurityConfig.class})
+public class TokenSchemaValidationIT extends TestContainersInitializerIT {
+
+    @Autowired
+    ReactiveMongoTemplate reactiveMongoTemplate;
+
+    Document token;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", () -> MONGO_CONTAINER.getReplicaSetUrl("authentication"));
+    }
+
+    @BeforeEach
+    void setup() {
+        token = TestUtilities.tokenDocument();
+    }
+
+    @Test
+    public void saveToken_validToken_Success() {
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+    @Test
+    public void saveToken_MissingComplytClientId_throwsValidationError() {
+        token.remove("complytClientId");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingComplytClientSecret_throwsValidationError() {
+        token.remove("complytClientSecret");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingAccessToken_throwsValidationError() {
+        token.remove("accessToken");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingAccessTokenIv_throwsValidationError() {
+        token.remove("accessTokenIv");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingScope_throwsValidationError() {
+        token.remove("scope");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingScopeIv_throwsValidationError() {
+        token.remove("scopeIv");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingExpiresIn_throwsValidationError() {
+        token.remove("expiresIn");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingTokenType_throwsValidationError() {
+        token.remove("tokenType");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingCreatedAt_throwsValidationError() {
+        token.remove("createdAt");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_MissingExpireAt_throwsValidationError() {
+        token.remove("expireAt");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidComplytClientIdType_throwsValidationError() {
+        token.put("complytClientId", 123);
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidComplytClientSecretType_throwsValidationError() {
+        token.put("complytClientSecret", new Document());
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidAccessTokenType_throwsValidationError() {
+        token.put("accessToken", false);
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidAccessTokenIvType_throwsValidationError() {
+        token.put("accessTokenIv", 456);
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidScopeType_throwsValidationError() {
+        token.put("scope", new Object());
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidScopeIvType_throwsValidationError() {
+        token.put("scopeIv", 789);
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidExpiresInType_throwsValidationError() {
+        token.put("expiresIn", "notAnInteger");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidTokenType_throwsValidationError() {
+        token.put("tokenType", 123);
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidCreatedAtType_throwsValidationError() {
+        token.put("createdAt", "notADate");
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidExpireAtType_throwsValidationError() {
+        token.put("expireAt", 123456);
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void saveToken_InvalidClassType_throwsValidationError() {
+        token.put("_class", 123);
+        StepVerifier.create(reactiveMongoTemplate.save(token, "token"))
+                .expectError()
+                .verify();
+    }
+}
