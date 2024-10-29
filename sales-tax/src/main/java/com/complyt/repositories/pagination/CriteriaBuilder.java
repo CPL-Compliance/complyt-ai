bac4f254b0c85584745f3c0170e8f1481f291f16
EyalTamsot <48117460+eyalTam@users.noreply.github.com>
2024-10-29 14:07:55 +0200

    improved CriteriaBuilder

--- sales-tax/src/main/java/com/complyt/repositories/pagination/CriteriaBuilder.java
+++ sales-tax/src/main/java/com/complyt/repositories/pagination/CriteriaBuilder.java
@@ -11,11 +11,10 @@ public interface CriteriaBuilder {
     static Criteria build(Map<String, String> filterMap, List<String> filterKeys) {
         List<Criteria> criterias = new ArrayList<>();
 
-        for (String key : filterMap.keySet()) {
-            if (filterKeys.contains(key)) {
-                criterias.add(Criteria.where(key).is(filterMap.get(key)));
-            }
-        }
+        filterMap.keySet().stream()
+                .filter(filterKeys::contains)
+                .map(key -> Criteria.where(key).is(filterMap.get(key)))
+                .forEach(criterias::add);
 
         return !criterias.isEmpty() ? new Criteria().andOperator(criterias.toArray(new Criteria[0])) : null;
     }
