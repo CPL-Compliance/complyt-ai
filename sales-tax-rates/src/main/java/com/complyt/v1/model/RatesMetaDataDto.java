722939ac7315dfe0179a58cef00d38e8ded6297f
Raz Adani <152158492+RazAdani@users.noreply.github.com>
2024-12-10 14:55:11 +0200

    Update RatesMetaDataDto.java (#525)
    * Update RatesMetaDataDto.java
    
    * annotation

--- sales-tax-rates/src/main/java/com/complyt/v1/model/RatesMetaDataDto.java
+++ sales-tax-rates/src/main/java/com/complyt/v1/model/RatesMetaDataDto.java
@@ -1,9 +1,11 @@
 package com.complyt.v1.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 
+@JsonInclude(JsonInclude.Include.NON_NULL) // Excludes null fields in JSON response
 @Schema(name = "RatesMetaData")
 public record RatesMetaDataDto(BigDecimal cityDistrictRate, BigDecimal countyDistrictRate) {
 }
--- sales-tax/src/main/java/com/complyt/v1/models/tax/sales_tax/RatesMetaDataDto.java
+++ sales-tax/src/main/java/com/complyt/v1/models/tax/sales_tax/RatesMetaDataDto.java
@@ -1,9 +1,11 @@
 package com.complyt.v1.models.tax.sales_tax;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 
+@JsonInclude(JsonInclude.Include.NON_NULL) // Excludes null fields in JSON response
 @Schema(name = "RatesMetaData")
 public record RatesMetaDataDto(BigDecimal cityDistrictRate, BigDecimal countyDistrictRate, BigDecimal specialDistrictRate) {
 }
