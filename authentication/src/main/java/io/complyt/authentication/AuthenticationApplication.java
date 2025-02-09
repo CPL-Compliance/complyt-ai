30700a4194fac7508a1c7ee473c3aa4800b19361
YoavZinger <124673224+YoavZinger@users.noreply.github.com>
2025-02-09 15:21:18 +0200

    authentication: removed log files and upgrade spring-security (#559)

--- authentication/src/main/java/io/complyt/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/io/complyt/authentication/AuthenticationApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Generated
 public class AuthenticationApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
     }
+
 }
