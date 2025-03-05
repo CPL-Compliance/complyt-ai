3a4220018fada8e81ea47954941f746e31554062
NivKaplan <155367577+NivKaplan@users.noreply.github.com>
2025-03-05 11:41:57 +0200

    Niv/com 1129 fix currency conversion deployment (#578)

--- sales-tax/src/test/java/com/complyt/repositories/TransactionProjectionStageTest.java
+++ sales-tax/src/test/java/com/complyt/repositories/TransactionProjectionStageTest.java
@@ -56,6 +56,8 @@ public class TransactionProjectionStageTest {
         add("exchangeRateInfo.fxRate");
         add("exchangeRateInfo.fromCurrency");
         add("exchangeRateInfo.toCurrency");
+        add("isRefundLinked");
+        add("refundLinkedPercentage");
     }};
 
     @Test
