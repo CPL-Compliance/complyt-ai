af6954f0ad43c7a4e282c3f0bf8ef4bad6d54640
Joel-Haag <joelohaag@gmail.com>
2025-05-28 08:45:11 +0200

    tests for coverage

--- sales-tax/src/test/java/com/complyt/business/timestamps_injection/NexusDateApplyDateInitializerTest.java
+++ sales-tax/src/test/java/com/complyt/business/timestamps_injection/NexusDateApplyDateInitializerTest.java
@@ -3,20 +3,11 @@ package com.complyt.business.timestamps_injection;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.complyt.business.nexus.ISalesTaxTrackingDateDeterminer;
-import com.complyt.domain.nexus.SalesTaxTracking;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Test;
-import testUtils.unit_test.UnitTestUtilities;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -28,27 +19,11 @@ class NexusDateApplyDateInitializerTest {
     @InjectMocks
     NexusDateApplyDateInitializer dateInitializer;
 
-    SalesTaxTracking salesTaxTracking;
-    UnitTestUtilities testUtilities;
-
-    @BeforeEach
-    void setUp() {
-        testUtilities = new UnitTestUtilities(LocalDateTime.now(), UUID.randomUUID().toString());
-        salesTaxTracking = testUtilities.createSalesTaxTracking(UUID.randomUUID().toString());
-        LocalDateTime now = LocalDateTime.now();
-
-        when(dateDeterminer.getSalesTaxTrackingAppliedDate(any())).thenReturn(now);
-
-        salesTaxTracking.setPhysicalNexusTracker(
-                salesTaxTracking.getPhysicalNexusTracker().withEstablishedDate(now)
-        );
-    }
 
     @Test
-    void init_setsAppliedDateCorrectly() {
-        SalesTaxTracking updated = dateInitializer.init(salesTaxTracking);
+    void init_setsAppliedDate_NullSalesTaxTracking_ReturnsNullException() {
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> dateInitializer.init(null));
 
-        assertEquals(salesTaxTracking.getPhysicalNexusTracker().getEstablishedDate(), updated.getPhysicalNexusTracker().getEstablishedDate());
-        assertEquals(salesTaxTracking.getAppliedDate(), updated.getAppliedDate());
+        assertEquals("salesTaxTracking is marked non-null but is null", exception.getMessage());
     }
 }
