4590283e162842132faf2ef2f6192fc6b5559c8e
Joel-Haag <joelohaag@gmail.com>
2025-05-28 08:53:33 +0200

    tests for coverage

--- sales-tax/src/test/java/com/complyt/business/timestamps_injection/NexusDateApplyDateInitializerTest.java
+++ sales-tax/src/test/java/com/complyt/business/timestamps_injection/NexusDateApplyDateInitializerTest.java
@@ -3,11 +3,20 @@ package com.complyt.business.timestamps_injection;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.complyt.business.nexus.ISalesTaxTrackingDateDeterminer;
+import com.complyt.domain.nexus.SalesTaxTracking;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Test;
+import testUtils.unit_test.UnitTestUtilities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +28,26 @@ class NexusDateApplyDateInitializerTest {
     @InjectMocks
     NexusDateApplyDateInitializer dateInitializer;
 
+    SalesTaxTracking salesTaxTracking;
+    UnitTestUtilities testUtilities;
+
+
+    @Test
+    void init_setsAppliedDateCorrectly() {
+        testUtilities = new UnitTestUtilities(LocalDateTime.now(), UUID.randomUUID().toString());
+        salesTaxTracking = testUtilities.createSalesTaxTracking(UUID.randomUUID().toString());
+        LocalDateTime now = LocalDateTime.now();
+
+        when(dateDeterminer.getSalesTaxTrackingAppliedDate(any())).thenReturn(now);
+
+        salesTaxTracking.setPhysicalNexusTracker(
+                salesTaxTracking.getPhysicalNexusTracker().withEstablishedDate(now)
+        );
+        SalesTaxTracking updated = dateInitializer.init(salesTaxTracking);
+
+        assertEquals(salesTaxTracking.getPhysicalNexusTracker().getEstablishedDate(), updated.getPhysicalNexusTracker().getEstablishedDate());
+        assertEquals(salesTaxTracking.getAppliedDate(), updated.getAppliedDate());
+    }
 
     @Test
     void init_setsAppliedDate_NullSalesTaxTracking_ReturnsNullException() {
