44bfa906c42b85cdd75329f70c062307d7592929
Eyal <eyalTams@gmail.com>
2025-07-09 11:27:03 +0300

    added test

--- processing-service/src/main/java/io/complyt/domain/ValidatedVat.java
+++ processing-service/src/main/java/io/complyt/domain/ValidatedVat.java
@@ -1,10 +1,12 @@
 package io.complyt.domain;
 
+import io.complyt.annotations.Generated;
 import io.complyt.domain.properties.InternalTimestampsProperty;
 import io.complyt.domain.timestamps.Timestamps;
 import lombok.With;
 
 @With
+@Generated
 public record ValidatedVat(
         String countryCode,
         String countryName,
